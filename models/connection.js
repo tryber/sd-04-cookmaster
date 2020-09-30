5b811bd1b54d5f0a77d04493798c812f89aca9d3
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-09-30 19:10:11 -0300

    cc v3

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 const mysqlx = require('@mysql/xdevapi');
 
-const connection = () => 
+const connection = () =>
   mysqlx
     .getSession({
       user: process.env.MYSQL_USER,
@@ -9,12 +9,10 @@ const connection = () =>
       host: process.env.HOSTNAME,
       port: 33060,
     })
-    .then((session) => session.getSchema('cookmaster')
-    )
-    .catch((err) => {
-      console.error(`deu ruim ${err}`);
+    .then((session) => session.getSchema('cookmaster'))
+    .catch((_err) => {
+      console.error('deu ruim');
       process.exit(1);
     });
-;
 
 module.exports = (connection);

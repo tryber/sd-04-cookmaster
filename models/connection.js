68adafa225a876fbb67b8d59e1a711129e228792
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-30 18:49:35 -0300

    CC fixing

--- models/connection.js
+++ models/connection.js
@@ -12,25 +12,25 @@ let schema;
 //   schema: 'cookmaster'
 // };
 
-function connection () {
+function connection() {
   return schema ?
-    Promisse.resolve(schema) :
+    Promise.resolve(schema) :
     mysqlx.getSession({
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       host: process.env.HOSTNAME,
       port: 33060,
       socketPath: '/var/run/mysqld/mysqld.sock',
-      schema: 'cookmaster'
+      schema: 'cookmaster',
     })
     .then((session) => {
       schema = session.getSchema('cookmaster');
       return schema;
     })
     .catch((err) => {
-      console.error(err);
+      console.err(err);
       process.exit(1);
-    })
+    });
 }
 
 module.exports = connection;

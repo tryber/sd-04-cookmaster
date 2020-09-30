fdb41d920ed32de33137fd67838282da760bcc59
caiolmf <caio.lmf@gmail.com>
2020-09-30 19:18:11 -0300

    add dotenv access where is necessary

---
+++ models/connections.js
@@ -0,0 +1,10 @@
+require('dotenv/config');
+
+const config = {
+  database: 'cookmaster',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};

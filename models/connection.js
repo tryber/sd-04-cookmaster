e1a82e3d952161cc93775263e6d6628ae67cddf1
Orlando <programadorlando@gmail.com>
2020-09-30 16:38:04 -0300

    initial configs + connection

---
+++ models/connection.js
@@ -0,0 +1,24 @@
+const mysqlx = require('@mysql/xdevapi');
+require('dotenv/config');
+
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+const connection = () => {
+  return mysqlx
+    .getSession(config)
+    .then(session => session.getSchema('cookmaster'))
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+module.exports = {
+  connection
+}

6a504a914cb463827445f29d182d8c23eee8f857
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-09-30 19:07:52 -0300

    cc v2

--- controllers/listAllControler.js
+++ controllers/listAllControler.js
@@ -3,7 +3,7 @@ const { getAll } = require('../models/listRecipesModel');
 const listRecipes = async (_req, res) => {
   const recipes = await getAll();
 
-  res.render('home', { recipes })
+  res.render('home', { recipes });
 };
 
 module.exports = {
--- models/connection.js
+++ models/connection.js
@@ -1,21 +1,20 @@
 require('dotenv/config');
 const mysqlx = require('@mysql/xdevapi');
 
-const connection = () => {
-  return mysqlx
+const connection = () => 
+  mysqlx
     .getSession({
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       host: process.env.HOSTNAME,
       port: 33060,
     })
-    .then((session) => {
-      return session.getSchema('cookmaster');
-    })
+    .then((session) => session.getSchema('cookmaster')
+    )
     .catch((err) => {
-      console.error("deu ruim");
+      console.error(`deu ruim ${err}`);
       process.exit(1);
     });
-};
+;
 
 module.exports = (connection);

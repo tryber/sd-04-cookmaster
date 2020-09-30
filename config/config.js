414bdfe74608772f08e8e8f07fd533d9d9f5098b
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-09-30 17:31:22 -0300

    obrigado pelos espaços

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD, 
-    "database": 'cookmaster', 
+    "password": process.env.MYSQL_PASSWORD,
+    "database": 'cookmaster',
     "host": process.env.HOSTNAME,
     "dialect": 'mysql',
   },

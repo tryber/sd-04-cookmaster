ae2af79bd13428f7feba559ed0e7c913546d2bb8
AliceAAtalla <aliceatalla36@gmail.com>
2020-09-30 18:08:24 -0300

    Functions findByEmail e findById

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,4 @@
-/* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
+const connection = require('./connection');
 
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
@@ -15,7 +8,22 @@ de fato, realize a busca no banco de dados */
  * @param {string} email Email do usuário a ser encontrado
  */
 const findByEmail = async (email) => {
-  return TEMP_USER;
+  const db = await connection();
+  const results = await db
+    .getTable(users)
+    .select(['id', 'email', 'password'])
+    .where('email = :email')
+    .bind('email', email)
+    .execute();
+
+  /* The method fetches all (or all remaining) rows of a query result set
+  and returns a list of tuples. If no more rows are available, it returns
+  an empty list. */
+  const user = await results
+    .fetchAll()
+    .then((email) => email[0].reduce((id, email, password) => ({ id, email, password })));
+
+  return user;
 };
 
 /**
@@ -23,7 +31,19 @@ const findByEmail = async (email) => {
  * @param {string} id ID do usuário
  */
 const findById = async (id) => {
-  return TEMP_USER;
+  const db = await connection();
+  const results = await db
+    .getTable('users')
+    .select(['id', 'email', 'password'])
+    .where('id = :id')
+    .bind('id', id)
+    .execute();
+
+  const user = await results
+    .fetchAll()
+    .then((id) => id[0].reduce((id, email, password) => ({ id, email, password })));
+
+  return user;
 };
 
 module.exports = {

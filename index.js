5071509134bf43ba0f003f7fd4b542f47851cbf4
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2020-09-30 13:20:08 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const middlewares = require('./middlewares');
+const controllers = require('./controllers');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (_req, res) => {
+  return res.render('home');
+});
+
+app.get('/admin', middlewares.auth(), (req, res) => {
+  return res.render('admin/home', { user: req.user });
+});
+
+app.get('/login', controllers.userController.loginForm);
+app.get('/logout', controllers.userController.logout);
+app.post('/login', controllers.userController.login);
+
+app.listen(3000, () => console.log('Listening on 3000'));

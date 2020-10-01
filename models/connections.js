2d7cea161f92b3e9e9d3bfbcaa74b546356848ba
caiolmf <caio.lmf@gmail.com>
2020-09-30 21:38:38 -0300

    create mysql connection function

--- models/connections.js
+++ models/connections.js
@@ -1,5 +1,9 @@
+const mysqlx = require('@mysql/xdevapi');
 require('dotenv/config');
 
+/**
+ * Mysql connection parameters
+ */
 const config = {
   database: 'cookmaster',
   user: process.env.MYSQL_USER,
@@ -8,3 +12,27 @@ const config = {
   port: 33060,
   socketPath: '/var/run/mysqld/mysqld.sock',
 };
+
+/**
+ * Mysql DB connection function
+ * return schema - Schema connection session
+ * return error - Schema connection error
+ */
+const connection = async () => {
+  let schema = null;
+
+  return schema
+    ? schema
+    : mysqlx
+        .getSession(config)
+        .then((session) => {
+          schema = session.getSchema(config.database);
+          return schema;
+        })
+        .catch((err) => {
+          console.log(err);
+          process.exit(1);
+        });
+};
+
+module.exports = connection;

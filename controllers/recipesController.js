4e4bc61750d67eb09df34afdc2909af54a05cbdc
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-10-01 00:43:52 -0300

    removido console.log

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -4,8 +4,6 @@ const { getRecipes } = require('../models/getRecipes');
 const recipesController = async (req, res) => {
   const recipes = await getRecipes();
   const { token = '' } = req.cookies || {};
-  console.log(token);
-  console.log(recipes)
   return res.render('home', { recipes, token });
 };
 

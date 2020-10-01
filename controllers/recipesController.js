857d8305d6b0cc83477a96c6893468825376503b
juniomelos <juniomelos@gmail.com>
2020-10-01 12:23:31 -0300

    req 01 - Expected to find content: 'Cookmaster' but never did.

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,8 +1,12 @@
 const { recipesModel } = require('../models');
 
 const listAllRecipes = async (req, res) => {
-  const getAllRecipes = await recipesModel.getAllRecipes();
-  return res.render('home', { getAllRecipes, user: req.user });
+  try {
+    const getAllRecipes = await recipesModel.getAllRecipes();
+    return res.render('home', { getAllRecipes, user: req.user });
+  } catch (error) {
+    return error;
+  }
 };
 
 module.exports = { listAllRecipes };

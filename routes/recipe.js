bdc8d720b5d1bf544e9bcdccaed926955fe0777f
caiolmf <caio.lmf@gmail.com>
2020-10-01 16:07:49 -0300

    add auth middlewares to recipe route '/'

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const middlewares = require('../middlewares')
 const recipeController = require('../controllers/recipe');
 
-router.get('/', recipeController.getRecipes);
+router.get('/', middlewares.auth(false), recipeController.getRecipes);
 
 module.exports = router;

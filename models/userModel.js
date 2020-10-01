f5b8982fd6dc50a55fe13198493acd9043c1eb07
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-10-01 19:12:10 -0300

    retirando funcao a mais

--- models/userModel.js
+++ models/userModel.js
@@ -10,7 +10,6 @@ const conn = require('./connection');
 
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
-const build = () => { }
 /**
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado

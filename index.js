d2b7c44e681a0090e317de1f7c0ea23ae1abc0f6
Rangel Paolo <paolorangel1996@outlook.com>
2020-10-01 15:20:33 -0300

    Criado compoente header

--- index.js
+++ index.js
@@ -26,6 +26,3 @@ app.get('/logout', controllers.userController.logout);
 app.post('/login', controllers.userController.login);
 
 app.listen(3000, () => console.log('Listening on 3000'));
-// connection().then((session) => {
-//   console.log('Conectado ao MySQL!');
-// });

0c0b811bb9abcbf1fd100e504b853c359674c142
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-10-01 17:56:54 -0300

    req 1?

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,12 +34,12 @@ const login = async (req, res, next) => {
   SESSIONS[token] = user.id;
 
   res.cookie('token', token, { httpOnly: true, sameSite: true });
-  res.redirect(redirect || '/admin');
+  res.redirect(redirect || '/');
 };
 
 const logout = (req, res) => {
   res.clearCookie('token');
-  if (!req.cookies || !req.cookies.token) return res.redirect('/login');
+  if (!req.cookies || !req.cookies.token) return res.redirect('/');
   res.render('admin/logout');
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,12 @@
-// const conn = require('./connection');
+const conn = require('./connection');
 /* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
+// const TEMP_USER = {
+//   id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
+//   email: 'taylor.doe@company.com',
+//   password: 'password',
+//   name: 'Taylor',
+//   lastName: 'Doe',
+// };
 
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
@@ -15,17 +15,45 @@ de fato, realize a busca no banco de dados */
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado
  */
-const findByEmail = async (email) => {
-  return TEMP_USER;
-};
+const findByEmail = async (email) =>
+  conn
+    .connection()
+    .then((db) =>
+      db.getTable('users').select(['id', 'email', 'password', 'first_name', 'last_name']).execute(),
+    )
+    .then((results) => results.fetchAll())
+    .then((users) =>
+      users.map(([id, email, password, name, lastName]) => ({
+        id,
+        email,
+        password,
+        name,
+        lastName,
+      })),
+    )
+    .then((res) => res.find((user) => user.email == email));
 
 /**
  * Busca um usuário através do seu ID
  * @param {string} id ID do usuário
  */
-const findById = async (id) => {
-  return TEMP_USER;
-};
+const findById = async (id) =>
+  conn
+    .connection()
+    .then((db) =>
+      db.getTable('users').select(['id', 'email', 'password', 'first_name', 'last_name']).execute(),
+    )
+    .then((results) => results.fetchAll())
+    .then((users) =>
+      users.map(([id, email, password, name, lastName]) => ({
+        id,
+        email,
+        password,
+        name,
+        lastName,
+      })),
+    )
+    .then((res) => res.find((user) => user.id == id));
 
 module.exports = {
   findByEmail,

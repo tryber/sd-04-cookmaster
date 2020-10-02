f97016c034ed1cdcdeefbb4c2b84f6d81cd41477
Joao Soutto <joaosoutto@hotmail.com>
2020-10-02 18:52:18 -0300

    iniciando projeto

---
+++ models/connection.js
@@ -0,0 +1,26 @@
+const mysqlx = require('@mysql/xdevapi');
+require('dotenv/config');
+
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+let schema;
+
+const connection = async () => {
+  if (schema) return Promise.resolve(schema);
+  try {
+    const session = await mysqlx.getSession(config);
+    schema = await session.getSchema('cookmaster');
+    return schema;
+  } catch (err) {
+    console.error(err);
+    return process.exit(1);
+  }
+};
+
+module.exports = connection;

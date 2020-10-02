e0f0fe25ae3bba926faf6d7f8326d3c20e104164
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-10-01 23:06:46 -0300

    Doing with Baeta brother

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
+require('dotenv/config');
+
 module.exports = {
   "development": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
+    "username": process.env.MYSQL_ROOT,
+    "password": process.env.MYSQL_MlRl*1986#,
     "database": "cookmaster",
-    "host": process.env.HOSTNAME,
+    "host": process.env.LOCALHOST,
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
+    "username": process.env.MYSQL_ROOT,
+    "password": process.env.MYSQL_MlRl*1986#,
     "database": "cookmaster",
-    "host": process.env.HOSTNAME,
+    "host": process.env.LOCALHOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
+    "username": process.env.MYSQL_ROOT,
+    "password": process.env.MYSQL_MlRl*1986#,
     "database": "cookmaster",
-    "host": process.env.HOSTNAME,
+    "host": process.env.LOCALHOST,
     "dialect": "mysql"
   }
 };

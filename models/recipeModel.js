dd54da685e8cbc874240473707b44ee23dc2b9eb
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-10-02 19:21:02 -0300

    corrigin bug

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -23,23 +23,25 @@ const getById = async (ID) =>
   connection()
     .then((schema) => {
       const table = schema.getTable('recipes');
-      return table.select([]).where('id = :id').bind({ ID }).execute();
+      return table.select([]).where('id = :ID').bind({ ID }).execute();
     })
     .then((selectResult) => selectResult.fetchOne())
-    .then(([id, userId, user, name, ingredients, instructions]) => ({
-      id,
-      userId,
-      user,
-      name,
-      ingredients,
-      instructions,
-    }));
+    .then(([id, userId, user, name, ingredients, instructions]) => {
+      return {
+        id,
+        userId,
+        user,
+        name,
+        ingredients,
+        instructions,
+      };
+    });
 
 const getAllByUser = async (ID) =>
   connection()
     .then((schema) => {
       const table = schema.getTable('recipes');
-      return table.select([]).where('user_id = :userId').bind({ ID }).execute();
+      return table.select([]).where('user_id = :ID').bind({ ID }).execute();
     })
     .then((selectResult) => selectResult.fetchAll())
     .then((recipes) =>

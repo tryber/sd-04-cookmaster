2794f37f99f635e46496d7961cfe53dc73f97c95
Filipe de Castro <filipeyoga@gmail.com>
2020-10-02 06:05:09 -0300

    fix: unnecessary space

--- models/userModel.js
+++ models/userModel.js
@@ -11,7 +11,7 @@ const findByEmail = async (emailInput) => {
 const findById = async (idInput) => {
   const db = await connection();
   const table = await db.getTable('users');
-  const results = await table.select([]).where('id = :id').bind('id', idInput ).execute();
+  const results = await table.select([]).where('id = :id').bind('id', idInput).execute();
   const [id, email, password, name, lastName] = results.fetchOne();
   return { id, email, password, name, lastName };
 };

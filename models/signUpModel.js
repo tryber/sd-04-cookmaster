551e51e148a1b7d26bede80e63f746499bcb9d08
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-02 20:41:35 -0300

    Mudando a validaçao para um switch case

--- models/signUpModel.js
+++ models/signUpModel.js
@@ -1,26 +1,28 @@
 const connection = require('./connection');
 
+const emailValidator = /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i;
+const passwordValidator = /^(\d|\w){6,}$/;
+const namesValidator = /\w{3,}/;
+
 const validation = ({ email, password, passwordConfirmation, first_name, last_name }) => {
-  if (!/[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i.test(email)) {
-    return { message: 'O email deve ter o formato email@mail.com' };
-  }
-  if (!/^(\d|\w){6,}$/.test(password)) {
-    return { message: 'A senha deve ter pelo menos 6 caracteres' };
-  }
-  if (password !== passwordConfirmation) {
-    return { message: 'As senhas tem que ser iguais' };
-  }
-  if (!/\w{3,}/.test(first_name)) {
-    return {
-      message: 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
-    };
-  }
-  if (!/\w{3,}/.test(last_name)) {
-    return {
-      message: 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
-    };
-  } else {
-    return { message: 'Cadastro efetuado com sucesso!' };
+  switch (true) {
+    case !emailValidator.test(email):
+      return { message: 'O email deve ter o formato email@mail.com' };
+    case !passwordValidator.test(password):
+      return { message: 'A senha deve ter pelo menos 6 caracteres' };
+    case password !== passwordConfirmation:
+      return { message: 'As senhas tem que ser iguais' };
+    case !namesValidator.test(first_name):
+      return {
+        message: 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
+      };
+    case !namesValidator.test(last_name):
+      return {
+        message: 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
+      };
+    default:
+      return { message: 'Cadastro efetuado com sucesso!' };
+      break;
   }
 };
 

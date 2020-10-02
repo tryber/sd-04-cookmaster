4a968e4fd3d974ab8cd0eae4abfc19c07ff057bc
caiolmf <caio.lmf@gmail.com>
2020-10-02 18:07:05 -0300

    add validation for passwor and retype match

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -4,11 +4,16 @@
  */
 const { body, validationResult } = require('express-validator');
 
+/**
+ * User data validation rules
+ */
 const userDataRules = () => [
   /** User email must be valid */
   body('email', 'O email deve ter o formato email@mail.com').exists().isEmail().normalizeEmail(),
   /** User password must be at last 6 characters long */
   body('password', 'A senha deve ter pelo menos 6 caracteres').exists().isLength({ min: 6 }),
+  /** User password and password retyne need to match */
+  body('passwordRetype', 'As senhas tem que ser iguais').contains(body('password')),
   /** user first name must be at least 3 characters long */
   body('firstName', 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras')
     .exists()
@@ -21,13 +26,15 @@ const userDataRules = () => [
     .isAlpha(),
 ];
 
-/** Do user data validation using given data validation rules on the route */
+/**
+ * Do user data validation using given data validation rules on the route*
+ */
 const userData = (req, res, next) => {
   const errors = validationResult(req);
 
-  if (errors) return next();
+  if (!errors) return next();
 
-  const normalizedErrors = errors.array().map((error) => ({ [error.param]: error.msg }));
+  const normalizedErrors = errors.array().map((error) => error.msg);
 
   return res.render('signup', { messages: normalizedErrors });
 };

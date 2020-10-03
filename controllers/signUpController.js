9b644b747952e9acd7f3f8246a97fae8a9ac64b2
Lucas Ribas <lucas@mundoribas.com.br>
2020-10-03 16:05:18 -0300

    Requisito 2 - tentativa 7

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -27,7 +27,7 @@ const messageNameCheck = (name, lastName) => {
 
 const messagesAllCheck = (email, password, confPassword, name, lastName) => {
   // tirado do https://emailregex.com/
-  const emailPattern = ^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailPattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   let message = 'Cadastro efetuado com sucesso!';
 

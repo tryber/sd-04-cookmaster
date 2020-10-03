4b21d46926daa19922e2fa7d663430f58513ebb6
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-10-03 17:51:09 -0300

    fixing cc issues

--- models/connection.js
+++ models/connection.js
@@ -1,23 +1,26 @@
-const mysqlx = require("@mysql/xdevapi");
+const mysqlx = require('@mysql/xdevapi');
 require('dotenv/config');
 
+let schema;
+
 const connection = () => {
-  return mysqlx
-    .getSession({
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      host: process.env.HOSTNAME,
-      port: 33060,
-      socketPath: '/var/run/mysqld/mysqld.sock',
-    })
-    .then((session) => {
-      schema = session.getSchema("cookmaster");
-      return schema;
-    })
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    });
+  return schema
+    ? Promise.resolve(schema)
+    : mysqlx
+        .getSession({
+          user: process.env.MYSQL_USER,
+          password: process.env.MYSQL_PASSWORD,
+          host: process.env.HOSTNAME,
+          port: 33060,
+          socketPath: '/var/run/mysqld/mysqld.sock',
+        })
+        .then((session) => {
+          schema = session.getSchema('cookmaster');
+          return schema;
+        })
+        .catch((err) => {
+          process.exit(1);
+        });
 };
 
 module.exports = connection;

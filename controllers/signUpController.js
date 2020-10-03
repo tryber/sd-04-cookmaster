cfc92c6c1e74a6522ffff90381e66891129d02e4
Orlando <programadorlando@gmail.com>
2020-10-03 10:53:14 -0300

    signUpController and userModel

---
+++ controllers/signUpController.js
@@ -0,0 +1,84 @@
+const userModel = require('../models/userModel');
+
+const renderSignup = async (_req, res) => {
+  res.render('admin/signUp', {
+    emailMessage: null,
+    passMessage: null,
+    confirmPass: null,
+    firstNamemessage: null,
+    lastNameMessage: null,
+    successMessage: null,
+  });
+};
+
+const handleEmailMessage = (email) => {
+  if (!userModel.emailIsValid(email)) {
+    return 'O email deve ter o formato email@mail.com';
+  }
+  return null;
+};
+
+const handlePassMessage = (password) => {
+  if (!userModel.passwordIsValid(password)) {
+    return 'A senha deve ter pelo menos 6 caracteres';
+  }
+  return null;
+};
+
+const handleConfirmPass = (password, confirmPass) => {
+  if (!userModel.confirmPass(password, confirmPass)) {
+    return 'As senhas tem que ser iguais"';
+  }
+  return null;
+};
+
+const handleFirstNameMessage = (firstName) => {
+  if (!userModel.nameIsValid(firstName)) {
+    return 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
+  }
+  return null;
+};
+
+const handleLastNameMessage = (lastName) => {
+  if (!userModel.nameIsValid(lastName)) {
+    return 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
+  }
+  return null;
+};
+
+const newUser = async (req, res) => {
+  const { email, password, confirmPass, firstName, lastName } = req.body;
+
+  const emailMessage = handleEmailMessage(email);
+  const passMessage = handlePassMessage(password);
+  const confirmPass = handleConfirmPass(password, confirmPass);
+  const firstNamemessage = handleFirstNameMessage(firstName);
+  const lastNameMessage = handleLastNameMessage(lastName);
+
+  if (emailMessage || passMessage || confirmPass || firstNamemessage || lastNameMessage) {
+    res.status(402).render('admin/signUp', {
+      emailMessage,
+      passMessage,
+      confirmPass,
+      firstNamemessage,
+      lastNameMessage,
+      successMessage: null,
+    });
+  }
+
+  await userModel.addUser(email, password, firstName, lastName);
+
+  res.status(201).render('admin/signUp', {
+    emailMessage,
+    passMessage,
+    confirmPass,
+    firstNamemessage,
+    lastNameMessage,
+    successMessage: 'Cadastro efetuado com sucesso!',
+  });
+};
+
+module.exports = {
+  renderSignup,
+  newUser,
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,5 @@
 const conn = require('./connection');
 
-/* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-// const TEMP_USER = {
-//   id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-//   email: 'taylor.doe@company.com',
-//   password: 'password',
-//   name: 'Taylor',
-//   lastName: 'Doe',
-// };
-
-/* Substitua o código das funções abaixo para que ela,
-de fato, realize a busca no banco de dados */
-
-/**
- * Busca um usuário através do seu email e, se encontrado, retorna-o.
- * @param {string} email Email do usuário a ser encontrado
- */
 const findByEmail = async (emailInput) => {
   const db = await conn();
   const result = await db
@@ -28,10 +12,6 @@ const findByEmail = async (emailInput) => {
   return { id, email, password, name, lastName };
 };
 
-/**
- * Busca um usuário através do seu ID
- * @param {string} id ID do usuário
- */
 const findById = async (idInput) => {
   const db = await conn();
   const table = await db
@@ -45,7 +25,29 @@ const findById = async (idInput) => {
   return { id, email, password, name, lastName };
 };
 
+const addUser = async(email, password, firstName, lastName) => {
+  const db = await conn();
+  return db
+    .getTable('users')
+    .insert(['email', 'password', 'first_name', 'last_name'])
+    .values(email, password, firstName, lastName)
+    .execute();
+};
+
+const emailIsValid = (email = '') => email.match(/\S+@\w+\.\w{2,6}(\.\w{2})?/i);
+
+const passwordIsValid = (password = '') => password.length > 5;
+
+const confirmPass = (password = '', confirmPass = '') => (password === confirmPass);
+
+const nameIsValid = (name = '') => name.match(/^\w{3,}/i);
+
 module.exports = {
   findByEmail,
   findById,
+  addUser,
+  emailIsValid,
+  passwordIsValid,
+  confirmPass,
+  nameIsValid,
 };

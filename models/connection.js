67584483d16fb0cc102b0eb4e89a9bebd4cf2786
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-10-03 17:56:18 -0300

    fixing cc issues

--- models/connection.js
+++ models/connection.js
@@ -3,24 +3,22 @@ require('dotenv/config');
 
 let schema;
 
-const connection = () => {
-  return schema
-    ? Promise.resolve(schema)
-    : mysqlx
-        .getSession({
-          user: process.env.MYSQL_USER,
-          password: process.env.MYSQL_PASSWORD,
-          host: process.env.HOSTNAME,
-          port: 33060,
-          socketPath: '/var/run/mysqld/mysqld.sock',
-        })
-        .then((session) => {
-          schema = session.getSchema('cookmaster');
-          return schema;
-        })
-        .catch((err) => {
-          process.exit(1);
-        });
-};
+function connection() {
+  return mysqlx
+    .getSession({
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.HOSTNAME,
+      port: 33060,
+      socketPath: '/var/run/mysqld/mysqld.sock',
+    })
+    .then((session) => {
+      schema = session.getSchema('cookmaster');
+      return schema;
+    })
+    .catch(() => {
+      process.exit(1);
+    });
+}
 
 module.exports = connection;

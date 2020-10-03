7f08400653a436266ae1481dc2e7d2a745e4c847
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-03 19:26:22 -0300

    fix/codeclimate

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mysqlx = require('@mysql/xdevapi');
 
-const connection = () => {
-  return mysqlx
+const connection = () =>
+  mysqlx
     .getSession({
       user: 'root',
       password: '11219205',
@@ -9,13 +9,9 @@ const connection = () => {
       port: 33060,
       schema: 'cookmaster',
     })
-    .then((session) => {
-      return session.getSchema('cookmaster');
-    })
+    .then((session) => session.getSchema('cookmaster'))
     .catch((err) => {
-      console.error(err);
       process.exit(1);
     });
-};
 
 module.exports = connection;
--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,13 @@
 const connection = require('./connection');
 
 /* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
+// const TEMP_USER = {
+//   id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
+//   email: 'taylor.doe@company.com',
+//   password: 'password',
+//   name: 'Taylor',
+//   lastName: 'Doe',
+// };
 
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
@@ -19,10 +19,13 @@ de fato, realize a busca no banco de dados */
 const findByEmail = async (userEmail) => {
   const userData = await connection()
     .then((db) =>
-      db.getTable('users').select([]).where('email = :email').bind('email', userEmail).execute(),
+      db.getTable('users')
+      .select([])
+      .where('email = :email')
+      .bind('email', userEmail)
+      .execute(),
     )
     .then((results) => results.fetchOne())
-    .catch((err) => console.error(err));
 
   const [id, email, password, name, lastName] = userData;
 
@@ -35,7 +38,11 @@ const findByEmail = async (userEmail) => {
  */
 const findById = async (userId) => {
   const userData = await connection()
-    .then((db) => db.getTable('users').select([]).where('id = :id').bind('id', userId).execute())
+    .then((db) => db.getTable('users')
+    .select([])
+    .where('id = :id')
+    .bind('id', userId)
+    .execute())
     .then((results) => results.fetchOne())
     .catch((err) => console.error(err));
 

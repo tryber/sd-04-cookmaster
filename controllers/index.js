4cebbad5612c024fd57c9081f3a6abcc2fa57252
Lucas Ribas <lucas@mundoribas.com.br>
2020-10-03 16:03:55 -0300

    Requisito 2 - tentativa 6

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
-const userController = require('./userController');
 const recipesController = require('./recipesController');
 const signUpController = require('./signUpController');
+const userController = require('./userController');
 
 module.exports = {
   recipesController,
--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,12 +1,21 @@
 const { createNewUser } = require('../models/userModel');
 
 const signUpForm = async (_req, res) => {
-  res.render('admin/signUp', { message: '' });
+  res.render('admin/signUp', {
+    emailMessage: null,
+    passMessage: null,
+    confirmPassMessage: null,
+    firstNameMessage: null,
+    lastNameMessage: null,
+    successMessage: null,
+  });
 };
 
 const messageNameCheck = (name, lastName) => {
   const namePattern = /\d/;
+
   let message;
+
   if (name.length < 3 || namePattern.test(name)) {
     message = 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
   }
@@ -16,14 +25,17 @@ const messageNameCheck = (name, lastName) => {
   return message;
 };
 
-const messagesAllCheck = (email, password, passwordConf, name, lastName) => {
-  const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const messagesAllCheck = (email, password, confPassword, name, lastName) => {
+  // tirado do https://emailregex.com/
+  const emailPattern = ^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   let message = 'Cadastro efetuado com sucesso!';
+
   const nameMessage = messageNameCheck(name, lastName);
   if (!emailPattern.test(email)) {
     message = 'O email deve ter o formato email@mail.com';
   }
-  if (password !== passwordConf) {
+  if (password !== confPassword) {
     message = 'As senhas tem que ser iguais';
   }
   if (password.length < 6) {
@@ -35,44 +47,10 @@ const messagesAllCheck = (email, password, passwordConf, name, lastName) => {
   return message;
 };
 
-// const messageEmail = (email) => {
-//   const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-//   let message = 'Cadastro efetuado com sucesso!';
-//   if (!emailPattern.test(email)) {
-//     message = 'O email deve ter o formato email@mail.com';
-//   }
-//   return message;
-// };
-
-// let messagePassword = (password, passwordConf) => {
-//   let message = 'Cadastro efetuado com sucesso';
-//   if (password !== passwordConf) {
-//     message = 'As senhas tem que ser iguais';
-//   }
-//   if (password.length < 6) {
-//     message = 'A senha deve ter pelo menos 6 caracteres';
-//   }
-//   return message;
-// };
-
-// const messagesAllCheck = (email, password, passwordConf, name, lastName) => {
-//   let message = 'Cadastro efetuado com sucesso';
-//   if (messageNameCheck(name, lastName)) {
-//     message = messageNameCheck;
-//   }
-//   if (messageEmail(email)) {
-//     message = messageEmail;
-//   }
-//   if ((messagePassword = (password, passwordConf))) {
-//     message = messagePassword;
-//   }
-//   return message;
-// };
-
 const signUp = async (req, res) => {
-  const { email, password, passwordConf, name, lastName } = req.body;
+  const { email, password, confPassword, name, lastName } = req.body;
 
-  const message = messagesAllCheck(email, password, passwordConf, name, lastName);
+  const message = messagesAllCheck(email, password, confPassword, name, lastName);
 
   createNewUser({
     email,

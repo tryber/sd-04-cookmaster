cf70434b1a4c71865bfa718d3b7c957435bd7756
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-10-03 15:32:21 -0300

    cc v1

--- controllers/listAllControler.js
+++ controllers/listAllControler.js
@@ -2,7 +2,7 @@ const { getAll } = require('../models/listRecipesModel');
 
 const listRecipes = async (req, res) => {
   const recipes = await getAll();
-  console.log(req.user)
+
   res.render('home', { recipes, user: req.user });
 };
 

3e52ca7606adec4343c49d5aca566f796977f9ee
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-02 21:36:48 -0300

    Colocando id no link

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -5,7 +5,7 @@ const getAllRecipes = async () => {
   const results = await db.getTable('recipes').select(['user', 'name']).execute();
 
   const listing = await results.fetchAll();
-  const list = await listing.map(([user, name]) => ({ user, name }));
+  const list = await listing.map(([id, user, name]) => ({ id, user, name }));
 
   return list;
 };

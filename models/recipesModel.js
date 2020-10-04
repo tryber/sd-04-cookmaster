80b0887ae122b3bfa054500575d90323bc2fd070
gabriellukke <gabriellukke@outlook.com>
2020-10-03 21:06:04 -0300

    Created Recipes Model to get all recipes from database

---
+++ models/recipesModel.js
@@ -0,0 +1,13 @@
+const connection = require('./connection');
+
+const getRecipes = async () => {
+  const db = await connection();
+  const table = await db.getTable('recipes');
+  const results = await table.select([]).execute();
+  const recipes = results.fetchAll();
+  return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({ id, userId, user, name, ingredients, instructions }));
+};
+
+module.exports = {
+  getRecipes,
+};

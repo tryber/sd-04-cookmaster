41f98753b809127ab8a6a50fe7143f085577ef01
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-10-05 15:23:56 -0300

    cc

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,13 +1,9 @@
-const { stringify } = require('uuid');
 const Recipe = require('../models/recipeModel');
 
 const searchRecipe = async (req, res) => {
   const user = req.user;
   const { q } = req.query;
-  console.log('q', q);
-  let recipes = await Recipe.searchRecipe(q);
-  const { f } = req.params;
-  await console.log('recipes', f);
+  const recipes = await Recipe.searchRecipe(q);
   res.render('search', { recipes, user });
 };
 

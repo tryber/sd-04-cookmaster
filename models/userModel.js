0949d286b70455be7578e8daa9e2f98e5fea816d
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-05 20:13:18 -0300

    evaluator bug

--- models/userModel.js
+++ models/userModel.js
@@ -9,9 +9,6 @@ const connection = require('./connection');
 //   lastName: 'Doe',
 // };
 
-/* Substitua o código das funções abaixo para que ela,
-de fato, realize a busca no banco de dados */
-
 /**
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado

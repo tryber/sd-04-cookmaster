089e795397ef78c76c1a0430c19d3258829c18dd
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-05 17:24:31 -0300

    re1/changed user login verification

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,9 +1,8 @@
 const recipesModel = require('../models/recipesModel');
 
 const listRecipes = async (req, res) => {
-  const { token = '' } = req.cookies || {};
   const recipes = await recipesModel.findAllRecipes();
-  res.render('home', { recipes, token });
+  res.render('home', { recipes, user: req.user });
 };
 
 module.exports = listRecipes;

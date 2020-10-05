bc61db2c34318b46aee133026b17964e49b223a0
Lucas Ribas <lucas@mundoribas.com.br>
2020-10-05 14:37:50 -0300

    Requisito 2 - tentativa 8

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -1,20 +1,13 @@
 const { createNewUser } = require('../models/userModel');
 
 const signUpForm = async (_req, res) => {
-  res.render('admin/signUp', {
-    emailMessage: null,
-    passMessage: null,
-    confirmPassMessage: null,
-    firstNameMessage: null,
-    lastNameMessage: null,
-    successMessage: null,
-  });
+  res.render('admin/signUp', {message: ''});
 };
 
 const messageNameCheck = (name, lastName) => {
   const namePattern = /\d/;
 
-  let message;
+  let message = '';
 
   if (name.length < 3 || namePattern.test(name)) {
     message = 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
@@ -27,19 +20,20 @@ const messageNameCheck = (name, lastName) => {
 
 const messagesAllCheck = (email, password, confPassword, name, lastName) => {
   // tirado do https://emailregex.com/
+  // the regex used in type=”email” from W3C
+  const nameMessage = messageNameCheck(name, lastName);
   const emailPattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   let message = 'Cadastro efetuado com sucesso!';
 
-  const nameMessage = messageNameCheck(name, lastName);
   if (!emailPattern.test(email)) {
     message = 'O email deve ter o formato email@mail.com';
   }
-  if (password !== confPassword) {
-    message = 'As senhas tem que ser iguais';
-  }
   if (password.length < 6) {
     message = 'A senha deve ter pelo menos 6 caracteres';
+  }
+  if (password !== confPassword) {
+    message = 'As senhas tem que ser iguais';
   }
   if (messageNameCheck(name, lastName)) {
     message = nameMessage;
@@ -52,7 +46,7 @@ const signUp = async (req, res) => {
 
   const message = messagesAllCheck(email, password, confPassword, name, lastName);
 
-  createNewUser({
+  await createNewUser({
     email,
     password,
     name,

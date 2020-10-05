9d8c76eb82c96a631b6191760f678890874cc8c1
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-10-05 17:39:13 -0300

    req 1, agora vai! eu acho

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', controllers.listRecipes.listRecipes);
+app.get('/', middlewares.auth(false), controllers.listRecipes.listRecipes);
 
 app.get('/admin', middlewares.auth(), (req, res) => {
   return res.render('admin/home', { user: req.user });

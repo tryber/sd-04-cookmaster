7eef243360ca69cb474c6d7116feb47e695ff89b
Thiago Tavares <thiagooo15@gmail.com>
2020-10-05 02:41:26 -0300

    quero dormir

--- models/connection.js
+++ models/connection.js
@@ -1,25 +1,27 @@
-// copiado https://github.com/tryber/sd-03-project-cookmaster/blob/lul-is-project-cookmaster/models/connection.js
-// motivos o CC não ta aceitando minhas push
 const mysqlx = require('@mysql/xdevapi');
 require('dotenv/config');
 
-let connection;
+let schema;
 
-module.exports = () =>
-  connection
-    ? Promise.resolve(connection)
-    : mysqlx
-        .getSession({
-          host: process.env.HOSTNAME,
-          user: process.env.MYSQL_USER,
-          password: process.env.MYSQL_PASSWORD,
-          port: '33060',
-          socketPath: '/var/run/mysqld/mysqld.sock',
-        })
-        .then(async (session) => {
-          connection = await session.getSchema('cookmaster');
-          return connection;
-        })
-        .catch(() => {
-          process.exit(1);
-        });
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+function connection() {
+  return schema ?
+    Promise.resolve(schema) :
+    mysqlx.getSession(config)
+    .then((session) => {
+      schema = session.getSchema('cookmaster');
+      return schema;
+    })
+    .catch(() => {
+      process.exit(1);
+    });
+}
+
+module.exports = connection;

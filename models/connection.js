a10618fbf7e60f43213ab0aa86220115c785aa4b
Thiago Tavares <thiagooo15@gmail.com>
2020-10-05 11:42:48 -0300

    c o cc nao passar...

--- models/connection.js
+++ models/connection.js
@@ -1,29 +1,20 @@
-require('dotenv/config');
+// copiei do https://github.com/tryber/sd-04-cookmaster/blob/gregorha-cookmaster/models/connection.js
 const mysqlx = require('@mysql/xdevapi');
+require('dotenv/config');
 
 const config = {
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   host: process.env.HOSTNAME,
+  db: process.env.MYSQL_DB,
   port: 33060,
   socketPath: '/var/run/mysqld/mysqld.sock',
 };
 
-let schema;
-
-const connection = () => {
-  return schema
-    ? Promise.resolve(schema)
-    : mysqlx
-        .getSession(config)
-        .then((session) => {
-          schema = session.getSchema('cookmaster');
-          return schema;
-        })
-        .catch((err) => {
-          console.error(err);
-          process.exit(1);
-        });
-};
+const connection = () =>
+  mysqlx
+    .getSession(config)
+    .then((session) => session.getSchema('cookmaster'))
+    .catch((err) => err);
 
 module.exports = connection;

94a45a64eeaf44a0084a742526d6678b4c7297fb
Thiago Tavares <thiagooo15@gmail.com>
2020-10-05 02:19:25 -0300

    vamo?

--- models/connection.js
+++ models/connection.js
@@ -12,11 +12,9 @@ const config = {
 let schema;
 
 const connection = () =>
-  schema
-    ? Promise.resolve(schema)
-    : mysqlx
-        .getSession(config)
-        .then((session) => (schema = session.getSchema('cookmaster')))
-        .catch(() => process.exit(1));
+  mysqlx
+    .getSession(config)
+    .then((session) => (schema = session.getSchema('cookmaster')))
+    .catch(() => process.exit(1));
 
 module.exports = connection;

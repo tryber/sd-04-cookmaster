169baf823e2f182bc9f7ead6e1c82a9cb9169a39
Joao Soutto <joaosoutto@hotmail.com>
2020-10-05 13:53:21 -0300

    req 3 CC

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -13,20 +13,20 @@ const recipeController = async (req, res) => {
 };
 
 const recipeDetailsController = async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
 
   const recipe = await Recipe.getRecipeById(id);
 
   const { token = '' } = req.cookies || {};
 
   try {
-    res.status(200).render('recipesDetails', {recipe, token});
+    res.status(200).render('recipesDetails', { recipe, token });
   } catch (err) {
     res.status(500).send('<h2>Não foi possivel realizar essa operação</h2>');
   }
-}
+};
 
 module.exports = {
   recipeController,
-  recipeDetailsController
+  recipeDetailsController,
 };

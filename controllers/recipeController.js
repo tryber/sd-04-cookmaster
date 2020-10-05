54c5a3a84814b67a2caea3866c1fe523cd4d3188
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-10-05 17:18:59 -0300

    cc

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,13 +1,11 @@
 const recipe = require('../models/recipeModel');
-const user = require('../models/userModel');
 
 const index = async (req, res) => {
   const recipes = await recipe.findAll();
-  // const usuario = await user.findByEmail('bruno.batista@gmail.com');
 
-  res.render("home", { recipes, user: req.user});
-}
+  res.render('home', { recipes, user: req.user });
+};
 
 module.exports = {
   index,
-}
+};
--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,12 +1,12 @@
 const connection = require('./connection');
 
-const findAll = async () => 
+const findAll = async () =>
   connection()
-  .then((db) => db.getTable('recipes').select(['user', 'name']).execute())
-  .then((results) => results.fetchAll())
-  .then((recipe) => recipe.map(([user, name]) => ({ user, name })))
-  .catch((err) => console.log("Erro findAll", err));  
+    .then((db) => db.getTable('recipes').select(['user', 'name']).execute())
+    .then((results) => results.fetchAll())
+    .then((recipe) => recipe.map(([user, name]) => ({ user, name })))
+    .catch((err) => console.log('Erro findAll', err));
 
 module.exports = {
   findAll,
-}
+};

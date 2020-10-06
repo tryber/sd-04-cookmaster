901681de4922204ce2bcf0d87728bb599ee9b9ca
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-10-06 18:50:23 -0300

    Sera?

--- models/connection.js
+++ models/connection.js
@@ -2,17 +2,26 @@ const mysqlx = require('@mysql/xdevapi');
 
 require('dotenv/config');
 
-const connection = () => (
-  mysqlx.getSession({
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    host: process.env.HOSTNAME,
-    port: 33060,
-    schema: 'cookmaster',
-    socketPath: '/var/run/mysqld/mysqld.sock',
-  })
-  .then((session) => session.getSchema('cookmaster'))
-  .catch(() => process.exit(1))
-);
+let schema;
+const connection = () => {
+  return schema
+    ? Promise.resolve(schema)
+    : mysqlx
+        .getSession({
+          user: process.env.MYSQL_USER,
+          password: process.env.MYSQL_PASSWORD,
+          host: process.env.HOSTNAME,
+          port: 33060,
+          schema: 'cookmaster',
+          socketPath: '/var/run/mysqld/mysqld.sock',
+        })
+        .then((session) => {
+          schema = session.getSchema('cookmaster');
+          return schema;
+        })
+        .catch(() => {
+          process.exit(1);
+        });
+};
 
 module.exports = connection;

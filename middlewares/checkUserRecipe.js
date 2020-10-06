5c990e944b6e7b67a972759b87f95e4486f7dba6
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-10-06 01:09:03 -0300

    requisito 7 refatorado

--- middlewares/checkUserRecipe.js
+++ middlewares/checkUserRecipe.js
@@ -3,7 +3,7 @@ const recipeModel = require('../models/recipesByIdModel');
 const checkUserRecipe = async (req, res, next) => {
   const recipe = await recipeModel(req.params.id);
   if (recipe[0][1] !== req.user.id) return res.redirect(`/recipes/${req.params.id}`);
-  next()
+  return next();
 };
 
 module.exports = checkUserRecipe;

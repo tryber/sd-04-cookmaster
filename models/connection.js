f40d26d2acb9a196cf1fcbca538593df0dc5be43
igorgbr <igorgphotoarte@gmail.com>
2020-10-06 13:56:54 -0300

    apesar de ser o 'padrao' o cc nao aceita

--- models/connection.js
+++ models/connection.js
@@ -10,7 +10,7 @@ const config = {
 };
 
 let schema; /* Aqui entra a variável que salva à conexão, começa como undefined */
-const connection = () => {
+function connection() {
   return schema /* Se schema já existir: */
     ? Promise.resolve(schema) /* Retorna o schema numa Promise: */
     : mysqlx
@@ -20,12 +20,11 @@ const connection = () => {
           schema = session.getSchema('cookmaster'); /* Armazenamos a conexão na variável `schema`*/
           return schema; /* E retornamos o schema de dentro da Promise */
         })
-        .catch((err) => {
+        .catch(() => {
           /* Caso um erro ocorra: */
-          console.error(err); /* Exibimos o erro no console */
           process.exit(1); /* E encerramos o processo */
         });
-};
+}
 
 /* function connection() {
   return mysqlx

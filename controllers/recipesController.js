34fa9673839ab9f0a2bff44b13adaec576f289bf
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-10-06 16:35:32 -0300

    delete recipe

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,4 +1,6 @@
+const { tables } = require('../models/connection');
 const { getAllRecipes, getRecipeById, updateRecipe } = require('../models/recipesModel');
+const { findById } = require('../models/userModel');
 
 const homeRecipes = async ({ userData = false }, res) => {
   const recipes = await getAllRecipes();
@@ -16,6 +18,22 @@ const editRecipe = async ({ params: { id }, userData }, res) => {
   return res.render('recipes/edit', { recipe, userData });
 };
 
+const deletePage = ({ params: { id } }, res) => res.render('recipes/delete', { id, message: '' });
+
+const deleteRecipe = async ({ params: { id }, userData, body }, res) => {
+  const recipe = await getRecipeById(id);
+
+  if (recipe.user_id !== userData.id) return res.redirect(`/recipes/${id}`);
+
+  const { password } = await findById(userData.id);
+
+  if (password !== body.password) return res.render('recipes/delete', { id, message: 'Senha incorreta' });
+
+  return tables.recipes((r) => r.delete().where('id = :id').bind('id', id).execute())
+    .then(() => res.redirect('/'))
+    .catch((err) => res.send(err));
+};
+
 const postRecipe = async ({ params: { id }, body }, res) => updateRecipe(id, body)
   .then(() => res.redirect('/'))
   .catch((err) => res.send(err));
@@ -25,4 +43,6 @@ module.exports = {
   oneRecipe,
   editRecipe,
   postRecipe,
+  deletePage,
+  deleteRecipe,
 };
--- index.js
+++ index.js
@@ -12,18 +12,21 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/admin', middlewares.auth(), (req, res) => res.render('admin/home', { user: req.userData }));
-app.get('/recipes/:id/edit', middlewares.auth(), recipesController.editRecipe);
-app.post('/recipes/:id', middlewares.auth(), recipesController.postRecipe);
-
 app.get('/login', userController.loginForm);
 app.get('/logout', userController.logout);
 app.get('/register', userController.register);
 app.post('/login', userController.login);
 
-app.use(middlewares.auth(false));
+app.get('/', middlewares.auth(false), recipesController.homeRecipes);
+app.get('/recipes/:id', middlewares.auth(false), recipesController.oneRecipe);
 
-app.get('/', recipesController.homeRecipes);
-app.get('/recipes/:id', recipesController.oneRecipe);
+app.use(middlewares.auth());
+
+app.get('/admin', (req, res) => res.render('admin/home', { user: req.userData }));
+app.get('/recipes/:id/edit', recipesController.editRecipe);
+app.get('/recipes/:id/delete', recipesController.deletePage);
+
+app.post('/recipes/:id/delete', recipesController.deleteRecipe);
+app.post('/recipes/:id', recipesController.postRecipe);
 
 app.listen(3000, () => console.log('Listening on 3000'));

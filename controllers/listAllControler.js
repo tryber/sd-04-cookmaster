d29d4bce12a2c78ed03df9db4116430be1f5be46
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-10-06 11:34:49 -0300

    cc v2

--- controllers/listAllControler.js
+++ controllers/listAllControler.js
@@ -12,7 +12,7 @@ const recipeDetails = async (req, res) => {
   const recipe = await getRecipeById(id);
 
   res.render('recipeDetails', { recipe, user: req.user });
-}
+};
 
 module.exports = {
   listRecipes,

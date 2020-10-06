23cc10351048f9c1c9129093ece89873f4d2ed4a
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-10-06 00:44:30 -0300

    Requisito 7 finalizado

--- controllers/recipesEditController.js
+++ controllers/recipesEditController.js
@@ -1,7 +1,7 @@
 const recipeModel = require('../models/recipesByIdModel');
 const recipeEditModel = require('../models/recipeEditModel');
 
-const recipeEditForm = async  (req, res) => {
+const recipeEditForm = async (req, res) => {
   const recipe = await recipeModel(req.params.id);
   if (recipe[0][1] !== req.user.id) return res.redirect(`/recipes/${req.params.id}`);
   return res.render('recipeEdit', { recipe, user: req.user });
@@ -10,11 +10,10 @@ const recipeEditForm = async  (req, res) => {
 const recipeEdit = async (req, res) => {
   const { recipeName, ingredients, recipeInstructions } = req.body;
   const ingredientsString = ingredients.join(',');
-  console.log()
-  
+
   recipeEditModel(req.params.id, recipeName, ingredientsString, recipeInstructions)
-  .then(() => res.redirect(`/recipes/${req.params.id}`));
-}
+    .then(() => res.redirect(`/recipes/${req.params.id}`));
+};
 
 module.exports = {
   recipeEditForm,

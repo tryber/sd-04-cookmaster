7538a28f79e43acd91c7a97ef7fbef6458dee649
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-06 15:28:45 -0300

    req3/added findRecipe|ById function

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -2,10 +2,11 @@ const connection = require('./connection');
 
 const findAllRecipes = async () => {
   const recipesData = await connection()
-    .then((db) => db.getTable('recipes').select(['user', 'name']).execute())
+    .then((db) => db.getTable('recipes').select(['id', 'user', 'name']).execute())
     .then((results) => results.fetchAll())
     .then((recipes) =>
-      recipes.map(([user, name]) => ({
+      recipes.map(([id, user, name]) => ({
+        id,
         user,
         name,
       })),
@@ -14,4 +15,19 @@ const findAllRecipes = async () => {
   return recipesData;
 };
 
-module.exports = { findAllRecipes };
+const findRecipeById = async (recipeId) => {
+  const recipeData = await connection()
+  .then((db) => db.getTable('recipes')
+  .select([])
+  .where('id = :id')
+  .bind('id', recipeId)
+  .execute()
+  .then((result) => result.fetchOne())
+  );
+
+  const [id, userId, user, name, ingredients, instructions] = recipeData;
+
+  return { id, userId, user, name, ingredients: ingredients.split(','), instructions };
+}
+
+module.exports = { findAllRecipes, findRecipeById };

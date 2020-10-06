f3dc902af75b3e1bfd82ffe156713dc50de1626b
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-06 18:27:10 -0300

    reverted connection refactor

--- models/connection.js
+++ models/connection.js
@@ -1,35 +1,35 @@
 const mysqlx = require('@mysql/xdevapi');
 
-// const connection = () =>
-//   mysqlx
-//     .getSession({
-//       user: 'root',
-//       password: '11219205',
-//       host: 'localhost',
-//       port: 33060,
-//       schema: 'cookmaster',
-//     })
-//     .then((session) => session.getSchema('cookmaster'))
-//     .catch(() => {
-//       process.exit(1);
-//     });
+const connection = () =>
+  mysqlx
+    .getSession({
+      user: 'root',
+      password: '11219205',
+      host: 'localhost',
+      port: 33060,
+      schema: 'cookmaster',
+    })
+    .then((session) => session.getSchema('cookmaster'))
+    .catch(() => {
+      process.exit(1);
+    });
 
-let schema;
-const connection = () => schema ? Promise.resolve(schema) :
-  mysqlx.getSession({
-    user: 'root',
-    password: '11219205',
-    host: 'localhost',
-    port: 33060,
-    schema: 'cookmaster',
-  })
-  .then((session) => {
-    schema = session.getSchema('cookmaster');
-    return schema;
-  })
-  .catch((err) => {
-    throw err;
-    process.exit(1);
-  });
+// let schema;
+// const connection = () => schema ? Promise.resolve(schema) :
+//   mysqlx.getSession({
+//     user: 'root',
+//     password: '11219205',
+//     host: 'localhost',
+//     port: 33060,
+//     schema: 'cookmaster',
+//   })
+//   .then((session) => {
+//     schema = session.getSchema('cookmaster');
+//     return schema;
+//   })
+//   .catch((err) => {
+//     throw err;
+//     process.exit(1);
+//   });
 
 module.exports = connection;

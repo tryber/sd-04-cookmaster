504f26e05abd8d98033ffb1a8cb47a0c7dfe287b
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-10-06 16:04:14 -0300

    verificando cc issues req 3

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,7 +2,7 @@ const recipeModel = require('../models/recipeModel');
 
 const showRecipes = async (req, res) => {
   const recipes = await recipeModel.findAll();
-  console.log(recipes);
+
   return res.render('home', { recipes, user: req.user });
   // tem q colocar tb o user para acessar na home e na navbar
 };

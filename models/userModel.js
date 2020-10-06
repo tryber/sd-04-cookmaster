e07b9f9b53768e6581c1ced914831b8075250278
gabriellukke <gabriellukke@outlook.com>
2020-10-06 15:20:40 -0300

    CC Fix v0.2

--- models/userModel.js
+++ models/userModel.js
@@ -6,11 +6,10 @@ const findByEmail = async (userEmail) => {
   const results = await table.select([]).where('email = :email').bind('email', userEmail).execute();
   const [id, email, password, name, lastName] = await results.fetchOne();
   return { id, email, password, name, lastName };
-
 };
 
 const findById = async (userId) => {
-  const db = await connection()
+  const db = await connection();
   const table = await db.getTable('users');
   const results = await table.select([]).where('id = :id').bind('id', userId).execute();
   const [id, email, password, name, lastName] = await results.fetchOne();

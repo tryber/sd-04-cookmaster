6dbf46fb9158d3df9417a10b4306fa7ccbe8065d
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-10-07 14:07:45 -0300

    edit recipe CC05

--- models/connection.js
+++ models/connection.js
@@ -4,27 +4,28 @@ require('dotenv').config();
 let schema; /* Aqui entra a variável que salva à conexão, começa como undefined */
 
 const connection = async () => {
-  return schema
-    ? Promise.resolve(schema)
-    : mysqlx
-        .getSession({
-          /* Se não, criamos uma nova conexão */ user: process.env.MYSQL_USER,
-          password: process.env.MYSQL_PASSWORD,
-          host: process.env.HOSTNAME,
-          port: 33060,
-          socketPath: '/var/run/mysqld/mysqld.sock',
-          schema: 'cookmaster',
-        })
-        .then((session) => {
-          /* Quando terminamos de abrir a conexão: */
-          schema = session.getSchema('cookmaster');
-          return schema; /* E retornamos o schema de dentro da Promise */
-        })
-        .catch(() => {
-          /* Caso um erro ocorra: */
-          // console.error(err); /* Exibimos o erro no console */
-          process.exit(1); /* E encerramos o processo */
-        });
+  if (schema) {
+    return Promise.resolve(schema);
+  }
+  return mysqlx
+    .getSession({
+      /* Se não, criamos uma nova conexão */ user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.HOSTNAME,
+      port: 33060,
+      socketPath: '/var/run/mysqld/mysqld.sock',
+      schema: 'cookmaster',
+    })
+    .then((session) => {
+      /* Quando terminamos de abrir a conexão: */
+      schema = session.getSchema('cookmaster');
+      return schema; /* E retornamos o schema de dentro da Promise */
+    })
+    .catch(() => {
+      /* Caso um erro ocorra: */
+      // console.error(err); /* Exibimos o erro no console */
+      process.exit(1); /* E encerramos o processo */
+    });
 };
 
 // const connection = () => {

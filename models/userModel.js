391f54b428abc51a03a808a2617a7de9311fd94d
Caio Ribeiro <cacoribeiro@gmail.com>
2020-10-07 13:39:32 -0300

    Iniciando Projeto

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,13 @@
 /* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
+// const TEMP_USER = {
+//   id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
+//   email: 'taylor.doe@company.com',
+//   password: 'password',
+//   name: 'Taylor',
+//   lastName: 'Doe',
+// };
 
+const connection = require();
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
 
@@ -15,7 +16,7 @@ de fato, realize a busca no banco de dados */
  * @param {string} email Email do usuário a ser encontrado
  */
 const findByEmail = async (email) => {
-  return TEMP_USER;
+  return TEMP_U;
 };
 
 /**

755e4e6d89e68ec5c2fa0f552379dd28dca962c1
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-10-07 00:13:15 -0300

    Requisito 9 finalizado

--- models/meEditModel.js
+++ models/meEditModel.js
@@ -24,7 +24,7 @@ const meEditModel = (id, email, password, firstName, lastName) =>
             .where('user_id = :id')
             .bind('id', id)
             .execute(),
-        )
+        ),
     );
 
 module.exports = meEditModel;

afd9b15b26808099b972147ce1190b68ddde0352
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-10-07 15:31:40 -0300

    segunda 2

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,7 +2,6 @@ const recipes = require('../models/recipesModel');
 
 const listRecipes = async (req, res) => {
   const recipe = await recipes.getRecipes();
-  console.log(recipe);
   res.render('home', { recipe });
 };
 
--- models/connection.js
+++ models/connection.js
@@ -23,7 +23,7 @@ const connection = () => (
         })
         .catch((err) => {
           throw err;
-        });
+        })
 );
 
 module.exports = connection;

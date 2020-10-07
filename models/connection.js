cfa286446f70c022ce2eaa8f940bd8c56813e667
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-10-07 18:46:13 -0300

    added .env info do db connection

--- models/connection.js
+++ models/connection.js
@@ -3,9 +3,9 @@ const mysqlx = require('@mysql/xdevapi');
 const connection = () =>
   mysqlx
     .getSession({
-      user: 'root',
-      password: '11219205',
-      host: 'localhost',
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.MYSQL_HOST,
       port: 33060,
       schema: 'cookmaster',
     })
@@ -13,23 +13,5 @@ const connection = () =>
     .catch(() => {
       process.exit(1);
     });
-
-// let schema;
-// const connection = () => schema ? Promise.resolve(schema) :
-//   mysqlx.getSession({
-//     user: 'root',
-//     password: '11219205',
-//     host: 'localhost',
-//     port: 33060,
-//     schema: 'cookmaster',
-//   })
-//   .then((session) => {
-//     schema = session.getSchema('cookmaster');
-//     return schema;
-//   })
-//   .catch((err) => {
-//     throw err;
-//     process.exit(1);
-//   });
 
 module.exports = connection;

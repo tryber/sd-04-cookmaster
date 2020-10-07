c4da614d865472b23aaad936eb1cbf95eab18744
wistyk <leonardoxw@gmail.com>
2020-10-07 17:26:14 -0300

    CC

--- models/connection.js
+++ models/connection.js
@@ -9,7 +9,7 @@ const config = {
 };
 
 let schema; /* Aqui entra a variável que salva à conexão, começa como undefined */
-const connection = () =>
+const connection = () => (
   schema /* Se schema já existir: */
     ? Promise.resolve(schema) /* Retorna o schema numa Promise: */
     : mysqlx
@@ -19,10 +19,11 @@ const connection = () =>
           schema = session.getSchema('cookmaster'); /* Armazenamos a conexão na variável `schema`*/
           return schema; /* E retornamos o schema de dentro da Promise */
         })
-        .catch((err) => {
+        .catch(() => {
           /* Caso um erro ocorra: */
           /* console.error(err); Exibimos o erro no console */
           process.exit(1); /* E encerramos o processo */
-        });
+        })
+);
 
 module.exports = connection;

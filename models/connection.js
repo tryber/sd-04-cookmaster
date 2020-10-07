e564f19e68a5bd5b908ba1f2300f4967cb197513
Caio Ribeiro <cacoribeiro@gmail.com>
2020-10-07 17:45:25 -0300

    aGORA VAI

--- models/connection.js
+++ models/connection.js
@@ -14,10 +14,9 @@ function connection() {
   return schema /* Se schema já existir: */
     ? Promise.resolve(schema) /* Retorna o schema numa Promise: */
     : mysqlx.getSession(config).then((session) => {
-        /* Quando terminamos de abrir a conexão: */
-        schema = session.getSchema('cookmaster'); /* Armazenamos a conexão na variável `schema`*/
-        return schema; /* E retornamos o schema de dentro da Promise */
-      });
+      schema = session.getSchema('cookmaster'); /* Armazenamos a conexão na variável `schema`*/
+      return schema; /* E retornamos o schema de dentro da Promise */
+    });
   // .catch((err) => {
   //   /* Caso um erro ocorra: */
   //   console.log(err); /* Exibimos o erro no console */

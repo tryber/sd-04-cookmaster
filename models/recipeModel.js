e1c47dca585b62d4d4e4d13bd0413a4b6103049c
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-10-07 19:39:52 -0300

    CC 01

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -2,15 +2,26 @@ const connection = require('./connection');
 
 const findAll = async () =>
   connection()
-    .then((db) => db.getTable('recipes').select(['id', 'user', 'name']).execute())
+    .then((db) => db.getTable('recipes').select(['id', 'user', 'name'])
+    .execute())
     .then((results) => results.fetchAll())
     .then((recipe) => recipe.map(([id, user, name]) => ({ id, user, name })));
 
-const findOne = async (idInput) => 
+const findOne = async (idInput) =>
   connection()
-    .then((db) => db.getTable('recipes').select([]).where('id =:idInput').bind('idInput', idInput).execute())
+    .then((db) =>
+      db.getTable('recipes').select([]).where('id =:idInput').bind('idInput', idInput)
+      .execute(),
+    )
     .then((results) => results.fetchOne())
-    .then(([id, user_id, user, name, ingredients, instructions]) => ({ id, user_id, user, name, ingredients, instructions }));
+    .then(([id, userId, user, name, ingredients, instructions]) => ({
+      id,
+      userId,
+      user,
+      name,
+      ingredients,
+      instructions,
+    }));
 
 module.exports = {
   findOne,

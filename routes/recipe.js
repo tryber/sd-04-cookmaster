2514c5a77ae8c0dab4e49817ea8b9f46a23f947f
caiolmf <caio.lmf@gmail.com>
2020-10-08 16:24:34 -0300

    change /recipe routes to /recipes

--- routes/recipe.js
+++ routes/recipe.js
@@ -7,10 +7,10 @@ const controllers = require('../controllers');
 /** Recipe search */
 router.get('/recipes/search', middlewares.auth(false), controllers.recipeController.searchRecipe);
 /** Recipe details */
-router.get('/recipe/:id', middlewares.auth(false), controllers.recipeController.getRecipe);
+router.get('/recipes/:id', middlewares.auth(false), controllers.recipeController.getRecipe);
 /** New recipe */
-router.get('/recipe/new', middlewares.auth(false), controllers.recipeController.createRecipe);
-router.post('/recipe', middlewares.auth(true), controllers.recipeController.createRecipe);
+router.get('/recipes/new', middlewares.auth(false), controllers.recipeController.createRecipe);
+router.post('/recipes', middlewares.auth(true), controllers.recipeController.createRecipe);
 /** Recipes home */
 router.get('/', middlewares.auth(false), controllers.recipeController.getRecipes);
 

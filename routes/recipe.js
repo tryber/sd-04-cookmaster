65fe50e962aba2ab184813f61410201999c40a4f
caiolmf <caio.lmf@gmail.com>
2020-10-08 15:53:43 -0300

    add route for new recipes

--- routes/recipe.js
+++ routes/recipe.js
@@ -4,10 +4,14 @@ const router = express.Router();
 const middlewares = require('../middlewares');
 const controllers = require('../controllers');
 
+/** Recipe search */
 router.get('/recipes/search', middlewares.auth(false), controllers.recipeController.searchRecipe);
-
+/** Recipe details */
 router.get('/recipe/:id', middlewares.auth(false), controllers.recipeController.getRecipe);
-
+/** New recipe */
+router.get('/recipe/new', middlewares.auth(false), controllers.recipeController.createRecipe);
+router.post('/recipe', middlewares.auth(true), controllers.recipeController.createRecipe);
+/** Recipes home */
 router.get('/', middlewares.auth(false), controllers.recipeController.getRecipes);
 
 module.exports = router;

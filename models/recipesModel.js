b96e0f9fe1858e4aabecac4e4d9eb9fbfe1c9532
wistyk <leonardoxw@gmail.com>
2020-10-11 15:57:05 -0300

    reqs ok

---
+++ models/recipesModel.js
@@ -0,0 +1,24 @@
+const connection = require('./connection');
+
+const findAll = async () =>
+  connection()
+    .then((db) => db.getTable('recipes').select(['name', 'user', 'id' ]).execute())
+    .then((results) => results.fetchAll())
+    .then((recipes) => recipes.map(([name, user, id]) => ({ name, user, id })));
+
+const findById = async (id) =>
+  connection().then((db) =>
+    db
+      .getTable('recipes')
+      .select()
+      .where('id = :idBind')
+      .bind('idBind', id)
+      .execute()
+      .then((result) => result.fetchOne())
+      .then((recipe) => recipe),
+  );
+
+module.exports = {
+  findAll,
+  findById,
+};

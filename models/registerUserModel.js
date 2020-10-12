fda65b5e1453189c420d54cc48825b870d39665d
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-10-12 14:33:00 -0300

    1

---
+++ models/registerUserModel.js
@@ -0,0 +1,14 @@
+const connection = require('./connection');
+
+const registerUserModel = async (email, password, name, lastName) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table.insert([
+    'email', 'password', 'first_name', 'last_name',
+  ])
+  .values(email, password, name, lastName)
+  .execute();
+  // return result.getWarningsCount();
+};
+
+module.exports = registerUserModel;

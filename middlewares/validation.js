1e8086d3d7050c2338938fbf8b34e41eb9831bdf
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-10-12 18:53:35 -0300

    req 2 fix validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,29 +4,32 @@ const message = {
   email: 'O email deve ter o formato email@mail.com',
   password: 'A senha deve ter pelo menos 6 caracteres',
   confirmPass: 'As senhas tem que ser iguais',
-  name: 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
+  firstName: 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
+  lastName: 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
 };
 
 const validationMessage = (validationBool, type) => (validationBool ? null : message[type]);
 
 const validationMiddleware = (req, res, next) => {
-  const { email, password, passwordConfirm, firstName } = req.body;
+  const { email, password, passwordConfirm, firstName, lastName } = req.body;
   const { emailCheck, passwordCheck, confirmPassCheck, nameCheck } = userModel.isValid;
 
-  const emailValidation = emailCheck(email);
-  const passValidation = passwordCheck(password);
-  const confirmPassValidation = confirmPassCheck(password, passwordConfirm);
-  const nameValidation = nameCheck(firstName);
+  const emailV = emailCheck(email);
+  const passV = passwordCheck(password);
+  const confirmPassV = confirmPassCheck(password, passwordConfirm);
+  const fNameV = nameCheck(firstName);
+  const lNameV = nameCheck(lastName);
 
-  if (emailValidation && passValidation && confirmPassValidation && nameValidation) {
+  if (emailV && passV && confirmPassV && fNameV && lNameV) {
     req.isValid = true;
   } else req.isValid = false;
 
   req.messages = {
-    email: validationMessage(emailValidation, 'email'),
-    password: validationMessage(passValidation, 'password'),
-    confirmPassword: validationMessage(confirmPassValidation, 'confirmPass'),
-    name: validationMessage(nameValidation, 'name'),
+    email: validationMessage(emailV, 'email'),
+    password: validationMessage(passV, 'password'),
+    confirmPassword: validationMessage(confirmPassV, 'confirmPass'),
+    firstName: validationMessage(fNameV, 'firstName'),
+    lastName: validationMessage(fNameV, 'lastName'),
   };
   return next();
 };

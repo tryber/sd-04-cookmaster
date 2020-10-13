fad8038c1ab3e9726766464faf341601f73aed1a
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-10-12 21:26:50 -0300

    req 3

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -2,10 +2,31 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const db = await connection();
-  const results = await db.getTable('recipes').select(['user', 'name']).execute();
+  const results = await db.getTable('recipes').select(['id', 'user', 'name']).execute();
   const fetchList = await results.fetchAll();
-  const list = await fetchList.map(([user, name]) => ({ user, name }));
+  const list = await fetchList.map(([id, user, name]) => ({ id, user, name }));
 
   return list;
 };
-module.exports = { getAll };
+
+const getById = async (recipeId) => {
+  const db = await connection();
+  const results = await db
+    .getTable('recipes')
+    .select(['id', 'user_id', 'user', 'name', 'ingredients', 'instructions'])
+    .where('id = :id')
+    .bind('id', recipeId)
+    .execute();
+  const fetchResult = await results.fetchOne();
+  console.log(fetchResult);
+  const [_, userId, user, name, ingredients, instructions] = fetchResult;
+  return {
+    userId,
+    user,
+    name,
+    ingredients,
+    instructions,
+  };
+};
+
+module.exports = { getAll, getById };

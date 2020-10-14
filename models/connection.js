e5748fe0312d116e67af181bcd8cb13be394bcdb
djonata-dias <diasdjonata@gmail.com>
2020-10-14 17:02:58 -0300

    Criei o arquivo de conexão com o banco de dados

---
+++ models/connection.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const mysqlx = require('@mysql/xdevapi');
+
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  schema: 'cookmaster',
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+let schema;
+
+const connection = () =>
+  (schema ?
+    Promise.resolve(schema) :
+    mysqlx.getSession(config)
+      .then((session) => {
+        schema = session.getSchema('cookmaster');
+        return schema;
+      })
+      .catch((err) => {
+        console.error(err);
+        process.exit(1);
+      }));
+
+
+module.exports = connection;

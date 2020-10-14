9ae1ab289cf26a8cf3886022d791bb6b971127c2
deboracosilveira <deboracosilveira@gmail.com>
2020-10-14 09:53:17 -0300

    Config iniciais

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
+require('dotenv/config');
+
 module.exports = {
   "development": {
     "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "cookmaster",
+    "password": process.env.MYSQL_PASSWORD, 
+    "database": 'cookmaster', 
     "host": process.env.HOSTNAME,
-    "dialect": "mysql"
+    "dialect": 'mysql',
   },
   "test": {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
-    "database": "cookmaster",
+    "database": 'cookmaster',
     "host": process.env.HOSTNAME,
-    "dialect": "mysql"
+    "dialect": "mysql",
   },
   "production": {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
-    "database": "cookmaster",
+    "database": 'cookmaster',
     "host": process.env.HOSTNAME,
-    "dialect": "mysql"
-  }
+    "dialect": 'mysql',
+  },
 };

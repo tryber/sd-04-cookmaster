631707ea659e384e6f719a039785aea7585022c6
Thabata <dornelasthabata@gmail.com>
2020-10-15 15:23:07 -0300

    Projeto ate onde ja fiz

---
+++ controllers/recipeController.js
@@ -0,0 +1,78 @@
+const recipeModel = require('../models/recipeModel');
+const userModel = require('../models/userModel');
+
+const showRecipes = async (req, res) => {
+  const recipes = await recipeModel.findAll();
+
+  return res.render('home', { recipes, user: req.user });
+};
+
+const seeRecipe = async (req, res) => {
+  const { id } = req.params;
+  const recipe = await recipeModel.recipeById(id);
+
+  res.render('seerecipe', { ...recipe, user: req.user });
+};
+
+const searchRecipess = async (req, res) => {
+  const { q } = req.query;
+
+  if (q === '') return res.render('searchRecipe', { recipes: null, user: req.user });
+
+  const recipes = await recipeModel.searchRecipes(q);
+
+  return res.render('searchRecipe', { recipes, user: req.user });
+};
+
+const myRecipe = async (req, res) => {
+  const { id } = req.user;
+  const recipes = await recipeModel.myRecipeByUserId(id);
+  return res.render('admin/meRecipes', { recipes, user: req.user });
+};
+
+const deleteRecipeForm = async (req, res) => {
+  const { id } = req.user;
+  const { userId } = await recipeModel.recipeById(req.params.id);
+
+  if (userId !== id) res.redirect('/');
+
+  return res.render('admin/delete', { message: null, user: req.user, id: req.params.id });
+};
+
+const deleteRecipe = async (req, res) => {
+  const { id } = req.user;
+  const { password: senha } = req.body;
+  const { password } = await userModel.findById(id);
+
+  if (password !== senha) {
+    res.render('admin/delete', { message: 'Senha Incorreta.', user: req.user, id: req.params.id });
+  }
+
+  await recipeModel.deleteRecipe(req.params.id);
+
+  return res.redirect('/');
+};
+
+// créditos a explicação de Tereza
+const newRecipeForm = async (req, res) => res.render('admin/new', { user: req.user });
+
+const newRecipe = async (req, res) => {
+  const { recipeName, ingredients, instructions } = req.body;
+  const { name, lastName, id } = req.user;
+  const userFullName = `${name} ${lastName}`;
+
+  await recipeModel.addNewRecipe(id, userFullName, recipeName, ingredients, instructions);
+
+  return res.redirect('/');
+};
+
+module.exports = {
+  showRecipes,
+  seeRecipe,
+  searchRecipess,
+  myRecipe,
+  deleteRecipeForm,
+  deleteRecipe,
+  newRecipe,
+  newRecipeForm,
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,4 @@
-/* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
+const connection = require('./connection');
 
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
@@ -14,19 +7,58 @@ de fato, realize a busca no banco de dados */
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado
  */
-const findByEmail = async (email) => {
-  return TEMP_USER;
-};
-
+const findByEmail = async (email) =>
+  connection()
+    .then((db) =>
+      db
+        .getTable('users')
+        .select(['id', 'email', 'password', 'first_name', 'last_name'])
+        .where('email = :email')
+        .bind('email', email)
+        .execute(),
+    )
+    .then((result) => result.fetchAll()[0])
+    .then(([userId, userEmail, password, name, lastName]) => ({
+      id: userId,
+      email: userEmail,
+      password,
+      name,
+      lastName,
+    }));
 /**
  * Busca um usuário através do seu ID
  * @param {string} id ID do usuário
  */
-const findById = async (id) => {
-  return TEMP_USER;
-};
+const findById = async (id) =>
+  connection()
+    .then((db) =>
+      db
+        .getTable('users')
+        .select(['id', 'email', 'password', 'first_name', 'last_name'])
+        .where('id = :id')
+        .bind('id', id)
+        .execute(),
+    )
+    .then((result) => result.fetchAll()[0])
+    .then(([userId, userEmail, password, name, lastName]) => ({
+      id: userId,
+      email: userEmail,
+      password,
+      name,
+      lastName,
+    }));
+
+const registerUser = async ({ email, password, name, lastName }) =>
+  connection().then((db) =>
+    db
+      .getTable('users')
+      .insert(['email', 'password', 'first_name', 'last_name'])
+      .values(email, password, name, lastName)
+      .execute(),
+  );
 
 module.exports = {
   findByEmail,
   findById,
+  registerUser,
 };

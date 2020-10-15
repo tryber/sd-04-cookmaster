01f3a53c50481976ec3b7016eaf3b2943b663f3e
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-10-15 19:41:47 -0300

    findByEmail

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,5 @@
+const connection = require('./connection');
+
 /* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
 const TEMP_USER = {
   id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
@@ -14,16 +16,36 @@ de fato, realize a busca no banco de dados */
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado
  */
-const findByEmail = async (email) => {
-  return TEMP_USER;
+const findByEmail = async (emailParam) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table.select([]).where('email = :email').bind('email', emailParam).execute();
+
+  const user = result.fetchOne();
+
+  if (!user) return null;
+
+  const [id, email, password, name, lastName] = user;
+
+  return { id, email, password, name, lastName };
 };
 
 /**
  * Busca um usuário através do seu ID
  * @param {string} id ID do usuário
  */
-const findById = async (id) => {
-  return TEMP_USER;
+const findById = async (idParam) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table.select([]).where('email = :email').bind('email', idParam).execute();
+
+  const user = result.fetchOne();
+
+  if (!user) return null;
+
+  const [id, email, password, name, lastName] = user;
+
+  return { id, email, password, name, lastName };
 };
 
 module.exports = {

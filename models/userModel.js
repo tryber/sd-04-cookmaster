bf052fe2e8e05311a7d2d31b5979faa07fe977bc
Thabata <dornelasthabata@gmail.com>
2020-10-15 17:04:04 -0300

    userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,5 @@
 const connection = require('./connection');
 
-/* Substitua o código das funções abaixo para que ela,
-de fato, realize a busca no banco de dados */
-
 /**
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado

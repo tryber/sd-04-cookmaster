b144967bfb16e8b35b73c794dfcece27752eb286
Thabata <dornelasthabata@gmail.com>
2020-10-15 15:45:17 -0300

    COnfig

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv/config');
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //

bd6f0274f6d231189b84e79ea3ea3b85d9e9752f
Filipe de Castro <filipeyoga@gmail.com>
2020-10-16 08:47:19 -0300

    fix: delete unnused route

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
-// Projeto realizado com ajuda de colegas, arquitetura baseada no curso de nodejs do balta.io
+// Projeto realizado em pair programming com Diego Campos e ajuda do Orlando Messias e do Ronan
+// Arquitetura baseada no curso de nodejs do balta.io
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -18,12 +20,10 @@ app.use(express.static('./public'));
 // Carregando rotas
 const indexRoute = require("./routes/index-route");
 const meRoute = require("./routes/me-route");
-const signupRoute = require("./routes/me-route");
 const recipesRoute = require("./routes/recipes-route");
 
 app.use("/", indexRoute);
 app.use("/me", meRoute);
-app.use("/signup", signupRoute);
 app.use("/recipes", recipesRoute);
 
 app.listen(3000, () => console.log('Server is running on 3000'));
--- routes/signup-route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const signUpController = require('../controllers/signUpController');
-
-
-
-module.exports = router;

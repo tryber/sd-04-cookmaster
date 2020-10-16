262498e8ba26eb9405dd18da9ae8dddffe9ebcc3
gabriellukke <gabriellukke@outlook.com>
2020-10-16 02:57:58 -0300

    Search Recipes v0.2

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -1,41 +1,56 @@
 const connection = require('./connection');
 
 const getRecipes = async () => {
-  const db = await connection();
-  const table = await db.getTable('recipes');
-  const results = await table.select([]).execute();
-  const recipes = results.fetchAll();
-  return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({
-    id,
-    userId,
-    user,
-    name,
-    ingredients,
-    instructions,
-  }));
+  try{
+    const db = await connection();
+    const table = await db.getTable('recipes');
+    const results = await table.select([]).execute();
+    const recipes = results.fetchAll();
+    return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({
+      id,
+      userId,
+      user,
+      name,
+      ingredients,
+      instructions,
+    }));
+  }
+  catch {
+    return null;
+  }
 };
 
 const getRecipesById = async (inputId) => {
-  const db = await connection();
-  const table = await db.getTable('recipes');
-  const results = await table.select([]).where('id = :inputId').bind('inputId', inputId).execute();
-  const [id, userId, user, name, ingredients, instructions] = await results.fetchOne();
-  return { id, userId, user, name, ingredients, instructions };
+  try {
+    const db = await connection();
+    const table = await db.getTable('recipes');
+    const results = await table.select([]).where('id = :inputId').bind('inputId', inputId).execute();
+    const [id, userId, user, name, ingredients, instructions] = await results.fetchOne();
+    return { id, userId, user, name, ingredients, instructions };
+  }
+  catch {
+    return null;
+  }
 };
 
 const getRecipesByName = async (q) => {
-  const db = await connection();
-  const table = await db.getTable('recipes');
-  const results = await table.select([]).where('name like :q').bind('q', `%${q}%`).execute();
-  const recipes = await results.fetchAll();
-  return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({
-    id,
-    userId,
-    user,
-    name,
-    ingredients,
-    instructions,
-  }));
+  try {
+    const db = await connection();
+    const table = await db.getTable('recipes');
+    const results = await table.select([]).where('name like :q').bind('q', `%${q}%`).execute();
+    const recipes = await results.fetchAll();
+    return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({
+      id,
+      userId,
+      user,
+      name,
+      ingredients,
+      instructions,
+    }));
+  }
+  catch {
+    return null;
+  }
 };
 
 module.exports = {

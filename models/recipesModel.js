7f443fa5860cc19280e5913417cf6f82c71797e6
matheusysd <matheusyurisd@gmail.com>
2020-10-15 23:43:24 -0300

    recipesModel.js created, function getRecipes created

---
+++ models/recipesModel.js
@@ -0,0 +1,20 @@
+const connection = require('./connection');
+
+const getRecipes = async () => {
+  const db = await connection();
+  const table = await db.getTable('recipes');
+  const results = await table.select([]).execute();
+  const recipes = await results.fetchAll();
+  return recipes.map(([id, userId, user, recipe, ingredients, instructions]) => ({
+    id,
+    userId,
+    user,
+    recipe,
+    ingredients,
+    instructions,
+  }));
+};
+
+module.exports = {
+  getRecipes,
+};

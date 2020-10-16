cf52364845dbcf07a22aa147349a5465c696de77
djonata-dias <diasdjonata@gmail.com>
2020-10-16 10:48:10 -0300

    Criei tratativa de erro para rota desconhecida e disponibilizei os arquivos da pasta public para utilizar nas views para utilizar imagens e css

--- index.js
+++ index.js
@@ -4,26 +4,23 @@ const cookieParser = require('cookie-parser');
 const middlewares = require('./middlewares');
 const controllers = require('./controllers');
 
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', middlewares.auth(false), controllers.recipesController.allRecipes);
 
-app.get('/admin', middlewares.auth(), (req, res) => {
-  return res.render('admin/home', { user: req.user });
-});
+app.get('/admin', middlewares.auth(), (req, res) => res.render('admin/home', { user: req.user }));
 
 app.get('/login', controllers.userController.loginForm);
 app.get('/logout', controllers.userController.logout);
 app.post('/login', controllers.userController.login);
 
 app.get('*', (_req, res) => {
-  return res.status(404).json({ message: 'Pagina não encontrada' })
+  return res.status(404).json({ message: 'Pagina não encontrada' });
 });
 
 app.listen(3000);

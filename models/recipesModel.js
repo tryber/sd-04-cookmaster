dcf19ffe4e6c008803ef837bf435293818b5eeb0
gabriellukke <gabriellukke@outlook.com>
2020-10-16 03:59:38 -0300

    NO TRY CATCH

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -16,34 +16,26 @@ const getRecipes = async () => {
 };
 
 const getRecipesById = async (inputId) => {
-  try {
-    const db = await connection();
-    const table = await db.getTable('recipes');
-    const results = await table.select([]).where('id = :inputId').bind('inputId', inputId).execute();
-    const [id, userId, user, name, ingredients, instructions] = await results.fetchOne();
-    return { id, userId, user, name, ingredients, instructions };
-  } catch {
-    return null;
-  }
+  const db = await connection();
+  const table = await db.getTable('recipes');
+  const results = await table.select([]).where('id = :inputId').bind('inputId', inputId).execute();
+  const [id, userId, user, name, ingredients, instructions] = await results.fetchOne();
+  return { id, userId, user, name, ingredients, instructions };
 };
 
 const getRecipesByName = async (q) => {
-  try {
-    const db = await connection();
-    const table = await db.getTable('recipes');
-    const results = await table.select([]).where('name like :q').bind('q', `%${q}%`).execute();
-    const recipes = await results.fetchAll();
-    return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({
-      id,
-      userId,
-      user,
-      name,
-      ingredients,
-      instructions,
-    }));
-  } catch {
-    return null;
-  }
+  const db = await connection();
+  const table = await db.getTable('recipes');
+  const results = await table.select([]).where('name like :q').bind('q', `%${q}%`).execute();
+  const recipes = await results.fetchAll();
+  return recipes.map(([id, userId, user, name, ingredients, instructions]) => ({
+    id,
+    userId,
+    user,
+    name,
+    ingredients,
+    instructions,
+  }));
 };
 
 module.exports = {

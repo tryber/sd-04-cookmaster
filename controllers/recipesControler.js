27e2c621f9bb8b8cdf553b6d443d0401fc345c05
Thiago Tavares <thiagooo15@gmail.com>
2020-10-16 01:48:24 -0300

    ccfix

--- controllers/recipesControler.js
+++ controllers/recipesControler.js
@@ -1,14 +1,13 @@
 const recipesModel = require('../models/recipesModel');
-const Recipes = require('../models/recipesModel');
 
 const index = async (req, res) => {
-  const allRecipes = await Recipes.getAll();
+  const allRecipes = await recipesModel.getAll();
 
   res.render('home', { allRecipes, user: req.user });
 };
 
 const show = async (req, res) => {
-  const recipeDetails = await Recipes.getById(req.params.id);
+  const recipeDetails = await recipesModel.getById(req.params.id);
 
   res.render('recipes/details', { recipeDetails, user: req.user });
 };

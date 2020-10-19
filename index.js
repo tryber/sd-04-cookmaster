4a9a9ef5867bd73e42e93f4c2fc95e512be65e97
Tiago-ers <trsilva284@gmail.com>
2020-10-19 19:48:07 -0300

    requisito 9 cc2

--- index.js
+++ index.js
@@ -58,4 +58,4 @@ app.get('/login', controllers.userController.loginForm);
 app.get('/logout', controllers.userController.logout);
 app.post('/login', controllers.userController.login);
 
-app.listen(3001, () => console.log('Listening on 3000'));
+app.listen(3000, () => console.log('Listening on 3000'));

49878fe7f120b181659a798afca54db38f81a7de
Kaecio <kaecio@yahoo.com.br>
2020-10-19 00:38:34 -0300

    Req 2, CC

--- controllers/cadastroController.js
+++ controllers/cadastroController.js
@@ -5,7 +5,8 @@ const signUp = (_, res) => {
   res.render('register', { message: null });
 };
 
-const newUser = async (req, res) => { const validation = await validationsForms({ ...req.body });
+const newUser = async (req, res) => {
+  const validation = await validationsForms({ ...req.body });
   if (validation) {
     await userModel.createNewUser({ ...req.body });
     return res.render('register', { ...validation });

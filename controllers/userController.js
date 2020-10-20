5ac40499cb3b43ffb1d6d1111614a2a3847eed12
djonata-dias <diasdjonata@gmail.com>
2020-10-20 15:33:54 -0300

    Criei as validações conforme a regra de negocio

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,14 +39,46 @@ const login = async (req, res) => {
   return res.redirect(redirect || '/');
 };
 
-const logout = (req, res) => {
+const logout = async (req, res) => {
   res.clearCookie('token');
   if (!req.cookies || !req.cookies.token) return res.redirect('/login');
   return res.render('admin/logout');
+};
+const signupForm = (req, res) => {
+  return res.render('admin/signup', { message: null, redirect: null });
+};
+
+const signup = async (req, res) => {
+  const { name, lastname, email, password, agreePassword } = req.body;
+  const user = await userModel.findByEmail(email);
+  const resRender = (view, message, redirect) =>
+    res.render(view, { message, redirect: redirect || null });
+
+  const parseEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  if (parseEmail.test(user.email)) {
+    return resRender('admin/signup', 'O email deve ter o formato email@mail.com');
+  }
+
+  if (user.email) {
+    return resRender('admin/signup', 'Este email já foi cadastrado');
+  }
+
+  if (password.length < 6) {
+    return resRender('admin/signup', 'A senha deve ter pelo menos 6 caracteres');
+  }
+
+  if (password !== agreePassword) {
+    return resRender('admin/signup', 'As senhas tem que ser iguais');
+  }
+
+  userModel.addUser(name, lastname, email, password);
+  return resRender('admin/signup', 'Cadastro efetuado com sucesso', 'admin/login');
 };
 
 module.exports = {
   login,
   loginForm,
   logout,
+  signup,
+  signupForm,
 };

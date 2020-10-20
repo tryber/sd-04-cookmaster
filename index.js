7c5a6d6f4347bd2eef32eb9f20d3d2dde748256c
djonata-dias <diasdjonata@gmail.com>
2020-10-20 19:48:08 -0300

    Troquei a rota de signup para cadastro

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/', middlewares.auth(false), controllers.recipesController.allRecipes);
 app.get('/admin', middlewares.auth(), (req, res) => res.render('admin/home', { user: req.user }));
 
 app.get('/login', controllers.userController.loginForm);
-app.post('/signup', controllers.userController.signup);
-app.get('/signup', controllers.userController.signupForm);
+app.post('/cadastro', controllers.userController.signup);
+app.get('/cadastro', controllers.userController.signupForm);
 app.get('/logout', controllers.userController.logout);
 app.post('/login', controllers.userController.login);
 

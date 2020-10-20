bb8fcceab53a4d7bb39287c6215ac1d0df7ee232
djonata-dias <diasdjonata@gmail.com>
2020-10-20 18:39:39 -0300

    Mudei a construção do model para ir por etapas e retornar o numero de warnings quando houver erro de conexão com o banco

--- models/userModel.js
+++ models/userModel.js
@@ -4,47 +4,31 @@ const connection = require('./connection');
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} emailParam Email do usuário a ser encontrado
  */
-const findByEmail = async (emailParam) =>
-  connection()
-    .then((db) =>
-      db
-        .getTable('users')
-        .select(['id', 'email', 'password', 'first_name', 'last_name'])
-        .where('email = :email_param')
-        .bind('email_param', emailParam)
-        .execute(),
-    )
-    .then((results) => results.fetchOne())
-    .then(([id, email, password, firstName, lastName]) => ({
-      id,
-      email,
-      password,
-      firstName,
-      lastName,
-    }))
-    .catch((err) => {
-      throw err;
-    });
-
+const findByEmail = async (emailParam) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table
+    .select([])
+    .where('email = :email_param')
+    .bind('email_param', emailParam)
+    .execute();
+  const user = result.fetchOne();
+  if (!user) return null;
+  const [id, email, password, firstName, lastName] = user;
+  return { id, email, password, firstName, lastName };
+};
 /**
  * Busca um usuário através do seu ID
  * @param {string} idParam ID do usuário
  */
-const findById = async (idParam) =>
-  connection()
-    .then((db) =>
-      db.getTable('users').select([]).where('id = :param_id').bind('param_id', idParam)
-      .execute(),
-    )
-    .then((results) => results.fetchOne())
-    .then(([id, email, password, firstName, lastName]) => ({
-      id,
-      email,
-      password,
-      firstName,
-      lastName,
-    }));
-
+const findById = async (idParam) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table.select([]).where('id = :param_id').bind('param_id', idParam).execute();
+  const user = result.fetchOne();
+  const [id, email, password, firstName, lastName] = user;
+  return { id, email, password, firstName, lastName };
+};
 /**
  * Adiciona um usuário
  * @param {string} email Email do usuário
@@ -52,24 +36,26 @@ const findById = async (idParam) =>
  * @param {string} firstName Primeiro nome do usuário
  * @param {string} lastName Sobrenome do usuário
  */
-const addUser = async (firstName, lastName, email, password) =>
-  connection().then((db) =>
-    db
-      .getTable('users')
-      .insert(['email', 'password', 'first_name', 'last_name'])
-      .values(email, password, firstName, lastName)
-      .execute(),
-  );
+const addUser = async (firstName, lastName, email, password) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table
+    .insert(['email', 'password', 'first_name', 'last_name'])
+    .values(email, password, firstName, lastName)
+    .execute();
+  return result.getWarningsCount();
+};
 
 /**
  * Deleta um usuário através do seu ID
  * @param {string} idParam ID do usuário
  */
-const deleteUser = async (idParam) =>
-  connection().then((db) =>
-    db.getTable('users').delete().where('id = :param_id').bind('param_id', idParam)
-    .execute(),
-  );
+const deleteUser = async (idParam) => {
+  const db = await connection();
+  const table = await db.getTable('users');
+  const result = await table.delete().where('id = :param_id').bind('param_id', idParam).execute();
+  return result.getWarningsCount();
+};
 
 module.exports = {
   findByEmail,

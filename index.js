2534837870fdf9b9d6d864a789682b7bc219b47d
Mateus Moreira <ashuraw@hotmail.com>
2020-10-21 18:25:27 -0300

    cookmaster 0.7.0

--- index.js
+++ index.js
@@ -12,37 +12,33 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', middlewares.auth(false), controllers.recipeController.listAllRecipes);
+app.get('/', controllers.recipeController.showRecipes);
 
-app.get('/admin', middlewares.auth(), (req, res) => {
-  return res.render('admin/home', { user: req.user });
-});
+app.get('/admin', middlewares.auth(), controllers.recipeController.showRecipesAdm);
+
+app.route('/user/new')
+  .get(controllers.userController.userForm)
+  .post(controllers.userController.addUser);
 
 app.get('/login', controllers.userController.loginForm);
 app.get('/logout', controllers.userController.logout);
 app.post('/login', controllers.userController.login);
 
-app
-  .route('/signUp')
-  .get(controllers.userController.signUp)
-  .post(controllers.userController.createUser);
-
-app.get('/recipes/search', middlewares.auth(false), controllers.recipeController.searchRecipe);
-
 app.get('/recipes/new', controllers.recipeController.recipeForm);
-app.post('/recipes', middlewares.auth(), controllers.recipeController.newRecipe);
+app.post('/recipes', middlewares.auth(), controllers.recipeController.addRecipe);
 
-app.get('/recipes/edit', controllers.recipeController.recipeEdit);
-app.get('/recipes/delete', controllers.recipeController.recipeDelete);
+app.get('/recipes/search', middlewares.auth(false), controllers.recipeController.recipeSearch);
+
+app.get('/recipes/edit', controllers.recipeController.recipeEdit); // ???
+app.get('/recipes/delete', controllers.recipeController.recipeDelete); // ???
+
+app.get('/recipes/:id', middlewares.auth(false), controllers.recipeController.showRecipeId);
 
 app.get('/recipes/:id/edit', middlewares.auth(), controllers.recipeController.recipeEditForm);
 app.post('/recipes/:id', middlewares.auth(), controllers.recipeController.recipeUpdate);
 
-
-app.get('/recipes/:id/delete', middlewares.auth(), controllers.recipeController.recipeDelete);
+app.get('/recipes/:id/delete', middlewares.auth(), controllers.recipeController.recipeDeleteForm);
 app.post('/recipes/:id/delete', middlewares.auth(), controllers.recipeController.recipeDel);
-
-app.get('/recipes/:id', middlewares.auth(false), controllers.recipeController.recipeDetail);
 
 app.get('/me/recipes', middlewares.auth(), controllers.recipeController.showUserRecipes);
 

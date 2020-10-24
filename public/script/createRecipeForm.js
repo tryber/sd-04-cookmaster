70c5aad87f303b754e15f59ccc44333f556a2d01
djonata-dias <diasdjonata@gmail.com>
2020-10-24 02:55:02 -0300

    Criei um script para adicionar receitas

---
+++ public/script/createRecipeForm.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+  const addIngredientBtn = document.getElementById('addIngredient');
+  const ingredientsList = document.getElementById('ingredientsList');
+  const ingredientesInput = document.getElementById('ingredientesInput');
+
+  addIngredientBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const button = document.createElement('button');
+
+    input.value = ingredientesInput.value;
+    input.name = 'ingredients[]';
+    input.readOnly = 'true';
+    input.className = 'border-0';
+    li.appendChild(input);
+
+    button.addEventListener('click', () => {
+      e.preventDefault();
+      li.remove();
+    });
+    button.style.marginLeft = '10px';
+    button.innerText = 'x';
+    button.className = 'btn-danger';
+    if (input.value) {
+      li.appendChild(button);
+    }
+
+    ingredientsList.appendChild(li);
+    ingredientesInput.value = '';
+  });
+};

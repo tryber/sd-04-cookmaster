710f903143ffef2d75ad4cf5e610f06f2b9361ba
djonata-dias <diasdjonata@gmail.com>
2020-10-24 01:05:30 -0300

    Criei o controler de pesquisa

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,13 +1,23 @@
 const recipesModel = require('../models/recipesModel');
 
 const allRecipes = async (req, res) => {
-  const recipes = await recipesModel.fetchAllRecipesModel();
-  res.render('home', { recipes, message: null, user: req.user });
+  const recipes = (await recipesModel.fetchAllRecipesModel()) || [];
+  return res.render('home', { recipes, message: null, user: req.user });
 };
 
 const recipePage = async (req, res) => {
   const { id } = req.params;
   const recipe = await recipesModel.fetchRecipeIdModel(id);
-  res.render('recipe', { recipe, message: null, user: req.user });
+  return res.render('recipe', { recipe, message: null, user: req.user });
 };
-module.exports = { allRecipes, recipePage };
+
+const searchRecipes = async (req, res) => {
+  const name = req.query.q;
+  let recipes = [];
+  if (name) {
+    recipes = await recipesModel.fetchRecipeNameModel(name);
+  }
+  return res.render('search', { recipes, message: null, user: req.user });
+};
+
+module.exports = { allRecipes, recipePage, searchRecipes };

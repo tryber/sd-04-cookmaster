5d72182f6eb268fce8b21ed3eb06ab6c298f5542
Luma Arruda <luma.oarruda@gmail.com>
2020-10-25 20:33:44 -0300

    connection

---
+++ models/connection.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const mysqlx = require('@mysql/xdevapi');
+
+let schema;
+const connection = () => {
+  return schema ?
+    Promise.resolve(schema) :
+    mysqlx.getSession({
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.HOSTNAME,
+      port: 33060,
+      socketPath: '/var/run/mysqld/mysqld.sock',
+    })
+    .then((session) => {
+      schema = session.getSchema('cookmaster');
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+};
+
+module.exports = connection;

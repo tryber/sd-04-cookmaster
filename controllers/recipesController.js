4564c9561ab3c3177b565fddf0020b325828fa3e
djonata-dias <diasdjonata@gmail.com>
2020-10-28 16:49:36 -0300

    Criei o recipe controller que adiciona uma nova receita com os dados obitidos do formulario

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -20,19 +20,35 @@ const searchRecipes = async (req, res) => {
   return res.render('search', { recipes, message: null, user: req.user });
 };
 
-const createRecipePage = async (req, res) => {
-  return res.render('createRecipe', { message: null, redirect: null, user: req.user });
-};
+const createRecipePage = async (req, res) =>
+  res.render('createRecipe', { message: null, redirect: null, user: req.user });
 
 const createRecipe = async (req, res) => {
-  // console.log(req.body);
-
   const {
-    body: { name, ingredients, method },
+    body: { name, ingredients, instructions },
     user,
   } = req;
-
-  return res.render('createRecipe', { message: 'Receita cadastrada com sucesso', redirect: null, user: req.user });
+  const warningCounts = recipesModel.insertRecipeIdModel(
+    user.id,
+    `${user.firstName} ${user.lastName}`,
+    name,
+    ingredients.join(),
+    instructions,
+  );
+  if (warningCounts > 0) {
+    res.status(400);
+    return res.render('createRecipe', {
+      message: 'Algum erro aconteceu e a receita não foi cadastrada!',
+      redirect: null,
+      user: req.user,
+    });
+  }
+  res.status(200);
+  return res.render('createRecipe', {
+    message: 'Receita cadastrada com sucesso!',
+    redirect: null,
+    user: req.user,
+  });
 };
 
 module.exports = { allRecipes, recipePage, searchRecipes, createRecipe, createRecipePage };

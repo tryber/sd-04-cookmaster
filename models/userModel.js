8c7637d9f32694078d6da0c327504be3abbe9c4f
binhus <eng.robsonwilli@gmail.com>
2020-10-27 23:18:16 -0300

    atualizando nodemon e aderindo as dicas do projeto

--- models/userModel.js
+++ models/userModel.js
@@ -16,17 +16,28 @@ de fato, realize a busca no banco de dados */
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado
  */
+// Abaixo está uma implementação do método `findByEmail`
 const findByEmail = async (email) => {
-  return TEMP_USER;
+  return connection()
+    .then((db) =>
+      db
+        .getTable('users')
+        .select(['id', 'email', 'password', 'first_name', 'last_name'])
+        .where('email = :email_param')
+        .bind('email_param', email)
+        .execute(),
+    )
+    .then((results) => results.fetchOne())
+    .then(([id, email, password, name, lastName]) => ({ id, email, password, name, lastName }));
 };
-
 /**
  * Busca um usuário através do seu ID
  * @param {string} id ID do usuário
  */
-const findById = async (id) => {
-  return TEMP_USER;
-};
+ 
+ const findById = async (id) => {
+   // implementar o método findById usando o método findByEmail como referência
+ }
 
 module.exports = {
   findByEmail,

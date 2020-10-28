112cbbdb51b44cf64b211f797d877a5a2765643d
Luma Arruda <luma.oarruda@gmail.com>
2020-10-28 17:05:17 -0300

    estabelecendo conecção com o banco de dados

--- models/connection.js
+++ models/connection.js
@@ -1,24 +1,29 @@
 require('dotenv').config();
 const mysqlx = require('@mysql/xdevapi');
 
-let schema;
-const connection = () => {
-  return schema ?
-    Promise.resolve(schema) :
-    mysqlx.getSession({
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      host: process.env.HOSTNAME,
-      port: 33060,
-      socketPath: '/var/run/mysqld/mysqld.sock',
-    })
-    .then((session) => {
-      schema = session.getSchema('cookmaster');
-      return schema;
-    })
-    .catch((err) => {
-      process.exit();
-    });
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: 'var/run/mysqld/mysqld.sock',
 };
+
+let schema;
+
+const connection = () =>
+  schema
+    ? Promise.resolve(schema)
+    : mysqlx
+      .getSession(config)
+      .then((session) => {
+        schema = session.getSchema('cookmaster');
+        return schema;
+      })
+      .catch((err) => {
+        console.log(err);
+        process.exit();
+      });
+;
 
 module.exports = connection;

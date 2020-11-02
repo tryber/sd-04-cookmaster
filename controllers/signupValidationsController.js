42b443734e27ac4e4999b7b0835c94796d005293
Luma Arruda <luma.oarruda@gmail.com>
2020-11-02 19:13:33 -0300

    cc

--- controllers/signupValidationsController.js
+++ controllers/signupValidationsController.js
@@ -1,39 +1,45 @@
 // https://regexr.com/3e48o
 const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+let message;
 
 const emailValidation = (email) => {
   if (!regex.test(email)) {
-    const message = 'O email deve ter o formato email@mail.com';
+    let message = 'O email deve ter o formato email@mail.com';
     return message;
   }
+  return message;
 };
 
 const passwordValidation = (password) => {
   if (password.length < 6) {
-    const message = 'A senha deve ter pelo menos 6 caracteres';
+    let message = 'A senha deve ter pelo menos 6 caracteres';
     return message;
   }
+  return message;
 };
 
 const passwordCheckValidation = (passwordCheck, password) => {
   if (passwordCheck !== password) {
-    const message = 'As senhas tem que ser iguais';
+    let message = 'As senhas tem que ser iguais';
     return message;
   }
+  return message;
 };
 
 const firstNameValidation = (firstName) => {
   if (firstName.length < 3 || typeof firstName !== 'string') {
-    const message = 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
+    let message = 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
     return message;
   }
+  return message;
 };
 
 const lastNameValidation = (lastName) => {
   if (lastName.length < 3 || typeof lastName !== 'string') {
-    const message = 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
+    let message = 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras';
     return message;
   }
+  return message;
 };
 
 module.exports = {

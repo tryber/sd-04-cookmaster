fb5dac7b1cc2a49dc6358c238f8cd1f07c321014
Luma Arruda <luma.oarruda@gmail.com>
2020-11-02 00:05:53 -0300

    login models

--- models/userModel.js
+++ models/userModel.js
@@ -1,34 +1,39 @@
 const connection = require('./connection')
 
-const findByEmail = async (userEmail) => {
-  const db = await connection();
-  const table = await db.getTable('users');
-  const result = await table.select().where('email = :email').bind('email', userEmail).execute();
-  const user = await result.fetchOne();
-  
-  if (!user) return null;
+const findByEmail = async (email) => 
+  connection()
+    .then((db) => db
+      .getTable('users')
+      .select()
+      .where('email = :email')
+      .bind('email', email)
+      .execute()
+    )
+    .then((results) => results.fetchOne())
+    .then(([id, email, password, firstName, lastName]) => ({
+      id,
+      email,
+      password,
+      firstName,
+      lastName
+    }));
 
-  const [id, email, password, firstName, lastName] = user;
-
-  return {
-    id, email, password, firstName, lastName,
-  };
-};
-
-const findById = async (userId) => {
-  const db = await connection();
-  const table = await db.getTable('users');
-  const result = await table.select().where('id = :id').bind('id', userId).execute();
-  const user = await result.fetchOne();
-  
-  if (!user) return null;
-
-  const [id, email, password, firstName, lastName] = user;
-
-  return {
-    id, email, password, firstName, lastName,
-  };
-};
+const findById = async (id) => connection()
+  .then((db) => db
+    .getTable('users')
+    .select()
+    .where('id = :id')
+    .bind('id', id)
+    .execute()
+  )
+  .then((results) => results.fetchOne())
+  .then(([id, email, password, firstName, lastName]) => ({
+    id,
+    email,
+    password,
+    firstName,
+    lastName
+  }));
 
 module.exports = {
   findByEmail,

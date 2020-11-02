7437ac2133f387b8677ed1bc0b7969abd700fb79
binhus <eng.robsonwilli@gmail.com>
2020-11-02 16:30:53 -0300

    ultimo errinho e ja era

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -24,9 +24,9 @@ const findRecipeById = (RecipeId) =>
     )
     .then((result) => result.fetchAll())
     .then((result) =>
-      result.map(([id, user_id, user, name, ingredients, instructions]) => ({
+      result.map(([id, user_Id, user, name, ingredients, instructions]) => ({
         id,
-        user_id,
+        user_Id,
         user,
         name,
         ingredients,

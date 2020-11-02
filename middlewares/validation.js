9b8be009be8af8800192a7dc2e70c3cbeb772017
Pedro <pedrotpoliveira@gmail.com>
2020-11-02 10:58:43 -0300

    Edit User - CC Fix

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,21 +1,21 @@
+const validator = require('validator');
+
 const validation = (req, res, next) => {
   const {
     email, password, confirm, name, lastname,
   } = req.body;
 
-  const emailPattern = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+  if (!validator.isEmail(email)) res.render('users/register', { error: 'email', success: false });
 
-  if (!emailPattern.test(email)) res.render('users/register', { error: 'email', success: false });
-
-  if (password.length < 6) res.render('users/register', { error: 'password', success: false });
+  if (!validator.isLength(password, { min: 6 })) res.render('users/register', { error: 'password', success: false });
 
   if (password !== confirm) res.render('users/register', { error: 'confirm', success: false });
 
-  if (!/^[a-z]+$/i.test(name) || name.length < 3) {
+  if (!validator.isLength(name, { min: 3 }) || !validator.isAlpha(name)) {
     return res.render('users/register', { error: 'name', success: false });
   }
 
-  if (!/^[a-z]+$/i.test(lastname) || lastname.length < 3) {
+  if (!validator.isLength(lastname, { min: 3 }) || !validator.isAlpha(lastname)) {
     return res.render('users/register', { error: 'lastname', success: false });
   }
 

5108c2ea8e208d0c1b55cad49c689738d0ac6fc5
Mateus Moreira Sarmento <60623845+Mateus-Moreira@users.noreply.github.com>
2020-11-03 09:58:47 -0300

    update

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,6 +1,7 @@
 const connection = require('./connection');
 const userModel = require('./userModel');
 
+
 const getAll = async () => {
   await connection()
     .then((db) => db.getTable('recipes'))

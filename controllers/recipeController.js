9f179fdd4bb2fa03c12d021855d2876264e2fbca
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-11-04 19:58:55 -0300

    Pagina de detalhes funcionando

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,32 +1,20 @@
 const Recipes = require('../models/recipesModel');
-const User = require('../models/userModel');
 
 const listRecipes = async (req, res) => {
   const recipe = await Recipes.getRecipes();
-  const user = req.user;
-
-  return res.render('home', { recipe, user });
+  return res.render('home', { recipe, user: req.user });
 };
 
 const recipeDetail = async (req, res) => {
-  Recipes.findById(req.params.id)
-    .then((rec) => res.render({ data: rec }))
-    .catch((r) => console.log(r));
-
-  // const { id } = req.params;
-  // const recipeDetail = await Recipes.findById(id);
-
-  // return res.render('recipeDetail', { recipeDetail, user: req.user });
+  const { id } = req.params;
+  const theRecipe = await Recipes.findById(id);
+  return res.render('recipeDetail', { theRecipe, user: req.user });
 };
 
-module.exports = { listRecipes, recipeDetail };
+const recipeName = async (req, res) => {
+  const { name } = req.params;
+  const recipeName = await Recipes.findByName(name);
+  return res.render('recipeDetail', { recipeName, user: req.user });
+};
 
-// const theRecipeName = async (req, res) => {
-//   const { name } = req.params;
-//   const recipeName = await Recipes.findByName(name);
-
-//   if (!recipeName) return res.status(404).render('404');
-
-//   res.render()
-
-// }
+module.exports = { listRecipes, recipeDetail, recipeName };

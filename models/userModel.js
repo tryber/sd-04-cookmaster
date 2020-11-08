ae3b314385cb1a190db3593bac9ec33585256a7b
fogo5000 <lucaslucas2008@gmail.com>
2020-11-08 09:39:37 -0300

    Requisition 1 done 1.1v

--- models/userModel.js
+++ models/userModel.js
@@ -1,22 +1,38 @@
 const connection = require('./connection');
 
-/* Substitua o código das funções abaixo para que ela,
-de fato, realize a busca no banco de dados */
-
-/**
- * Busca um usuário através do seu email e, se encontrado, retorna-o.
- * @param {string} email Email do usuário a ser encontrado
- */
-const findByEmail = async (email) => {
-  return TEMP_USER;
+const findByEmail = async (mail) => {
+  connection()
+    .then((dataBase) =>
+      dataBase
+        .getTable('users')
+        .select()
+        .where('email = :email')
+        .bind('email', mail)
+        .execute(),
+    )
+    .then((results) => results.fetchOne())
+    .then(([id, email, password, name, lastName]) => ({
+      id,
+      email,
+      password,
+      name,
+      lastName,
+    }));
 };
 
-/**
- * Busca um usuário através do seu ID
- * @param {string} id ID do usuário
- */
 const findById = async (id) => {
-  return TEMP_USER;
+  connection()
+    .then((dataBase) =>
+      dataBase.getTable('users').select(['id']).where('id = :id').bind('id', id).execute(),
+    )
+    .then((results) => results.fetchOne())
+    .then(([id, email, password, name, lastName]) => ({
+      id,
+      email,
+      password,
+      name,
+      lastName,
+    }));
 };
 
 module.exports = {
